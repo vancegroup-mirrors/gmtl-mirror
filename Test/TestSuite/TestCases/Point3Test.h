3751d7afe24341487583c5fd56458839fb4dccb3
allenb <allenb@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-01-26 23:10:48 +0000

    Updated test suite.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@15 6382ae23-ed94-47d5-a97d-470f0bc4535f

---
+++ Test/TestSuite/TestCases/Point3Test.h
@@ -0,0 +1,102 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: Point3Test.h,v $
+ * Date modified: $Date: 2002-01-26 23:10:48 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum 
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
+#include <gfxConfig.h>
+#include <iostream>
+
+#include <TestCase.h>
+#include <TestSuite.h>
+#include <TestCaller.h>
+
+#include <GMTL/Point3.h>
+
+namespace gmtlTest
+{
+
+class Point3Test : public TestCase
+{
+public:
+   Point3Test( std::string name )
+   : TestCase (name)
+   {;}
+
+   virtual ~Point3Test()
+   {}
+
+   virtual void setUp()
+   {;}
+
+   virtual void tearDown()
+   {;}
+
+   void testPoint3Creation()
+   {
+      gmtl::Point3  test_point;
+      test_point.set(1,2,3);
+
+      gmtl::Point3 new_point(test_point);    // Test copy constructor
+      gmtl::Point3 another_pt;
+      another_pt = test_point;               // Test it again
+      another_pt = new_point;
+
+      gmtl::Vec3 vec_pt(3,4,5);
+      gmtl::Point3 p1(vec_pt);
+      gmtl::Point3 p2;
+      p2 = vec_pt;
+      p1 = p2;
+      assertTest(p1 == vec_pt);           // Should be able to construct one
+
+   }
+
+
+
+   static Test* suite()
+   {
+      TestSuite* test_suite = new TestSuite ("Point3Test");
+      test_suite->addTest( new TestCaller<Point3Test>("testPoint3Creation", &Point3Test::testPoint3Creation));
+      return test_suite;
+   }
+
+   static Test* interactiveSuite()
+   {
+      TestSuite* test_suite = new TestSuite ("InteractiveThreadTest");
+      //test_suite->addTest( new TestCaller<ThreadTest>("interactiveCPUGrind", &ThreadTest::interactiveTestCPUGrind));
+      return test_suite;
+   }
+
+protected:
+
+};
+
+};
