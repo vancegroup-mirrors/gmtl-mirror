33125b108f7b62b634811f2a0b9e151cda54a03b
subatomic <subatomic@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-02-18 20:56:13 +0000

    added Type2Type
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@118 6382ae23-ed94-47d5-a97d-470f0bc4535f

---
+++ gmtl/Meta.h
@@ -0,0 +1,59 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: Meta.h,v $
+ * Date modified: $Date: 2002-02-18 20:56:13 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
+#ifndef _GMTL_METAPROGRAMMING_H
+#define _GMTL_METAPROGRAMMING_H
+
+namespace gmtl
+{  
+   /** A lightweight identifier you can pass to overloaded functions 
+    *  to typefy them.
+    *
+    *  Type2Type lets you transport the type information about T to functions
+    */
+   template <typename T>
+   struct Type2Type
+   {
+      typedef T OriginalType;
+   };
+   
+   /** Type2Type, 1 type, two ints...
+    */
+   template <typename T, unsigned U, unsigned V>
+   struct Type2TypeUU
+   {
+      typedef T OriginalType;
+   };
+} // end namespace
+#endif
