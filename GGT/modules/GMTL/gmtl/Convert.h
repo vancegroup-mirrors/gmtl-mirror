1a23fe1a5570090b503091a48b4a5adba3719a6e
subatomic <subatomic@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-02-20 17:40:39 +0000

    some initial work on the convert( mat, quat ) func (still commented out).
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@149 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/Convert.h
+++ GGT/modules/GMTL/gmtl/Convert.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Convert.h,v $
- * Date modified: $Date: 2002-02-15 22:14:27 $
- * Version:       $Revision: 1.1 $
+ * Date modified: $Date: 2002-02-20 17:40:39 $
+ * Version:       $Revision: 1.2 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -34,5 +34,57 @@
  ************************************************************ ggt-cpr end */
 #ifndef _GMTL_CONVERT_H_
 #define _GMTL_CONVERT_H_
+
+namespace gmtl
+{
+   /** convert a quaternion to the rotation part of a 3x3, 3x4, or 4x4 matrix. */
+   /*
+   template <typename DATA_TYPE, unsigned ROWS, unsigned COLS>
+   void convert( Matrix<DATA_TYPE, ROWS, COLS>& mat, const Quat<DATA_TYPE>& q )
+   {
+      assert( ((ROWS == 3 && COLS == 3) ||
+               (ROWS == 3 && COLS == 4) ||
+               (ROWS == 4 && COLS == 4)) && 
+               "pre conditions not met on convert( mat, quat )" );
+
+      // From Watt & Watt
+      DATA_TYPE wx, wy, wz, xx, yy, yz, xy, xz, zz, xs, ys, zs;
+      
+      xs = q[VJ_X] + q[VJ_X];    ys = q[VJ_Y] + q[VJ_Y];    zs = q[VJ_Z] + q[VJ_Z];
+      xx = q[VJ_X] * xs;   xy = q[VJ_X] * ys;   xz = q[VJ_X] * zs;
+      yy = q[VJ_Y] * ys;   yz = q[VJ_Y] * zs;   zz = q[VJ_Z] * zs;
+      wx = q[VJ_W] * xs;   wy = q[VJ_W] * ys;   wz = q[VJ_W] * zs;
+
+      mat( 0, 0 ) = 1.0 - (yy+zz);
+      mat( 1, 0 ) = xy+wz;
+      mat( 2, 0 ) = xz-wy;
+ 
+      mat( 0, 1 ) = xy-wz;
+      mat( 1, 1 ) = 1.0 - (xx+zz);
+      mat( 2, 1 ) = yz+wx;
+ 
+      mat( 0, 2 ) = xz+wy;
+      mat( 1, 2 ) = yz-wx;
+      mat( 2, 2 ) = 1.0 - (xx+yy);
+
+      if (ROWS == 4)
+      {
+         mat( 3, 0 ) = 0.0;
+         mat( 3, 1 ) = 0.0;
+         mat( 3, 2 ) = 0.0;
+      }
+
+      if (COLS == 4)
+      {
+         mat( 0, 3 ) = 0.0;
+         mat( 1, 3 ) = 0.0;
+         mat( 2, 3 ) = 0.0;
+      }
+
+      if (ROWS == 4 && COLS == 4)
+         mat( 3, 3 ) = 1.0;
+   }
+   */
+} // end namespace gmtl.
 
 #endif
