6b1f2e4b1ba498084b83c6c4a0abb5a3f4bc51e9
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-02-23 17:41:06 +0000

    Updated to cvs-gather version 0.0.10.  This version has extensive documentation additions (use perldoc to view the manpage) and the ability to update previously checked out modules in place.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@187 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/cvs-gather.pl
+++ GGT/cvs-gather.pl
@@ -24,7 +24,7 @@
 #
 # *************** <auto-copyright.pl END do not edit this line> ***************
 
-# $Id: cvs-gather.pl,v 1.4 2002-02-12 23:12:27 patrickh Exp $
+# $Id: cvs-gather.pl,v 1.5 2002-02-23 17:41:06 patrickh Exp $
 
 require 5.004;
 
@@ -32,6 +32,7 @@ use Cwd qw(chdir getcwd);
 use File::Basename;
 use File::Path;
 use Getopt::Long;
+use Pod::Usage;
 
 use strict 'vars';
 use vars qw($indent $log_file $full_path $debug_level);
@@ -39,7 +40,6 @@ use vars qw($CRITICAL_LVL $WARNING_LVL $CONFIG_LVL $STATE_LVL $VERB_LVL
             $HVERB_LVL $HEX_LVL);
 
 # Subroutine prototypes.
-sub printHelp();
 sub printVersion();
 sub parse($$);
 sub parseModule($$$;$);
@@ -49,15 +49,18 @@ sub expandWildcards($$$$$);
 sub doOverride($$);
 sub overrideValue($$$);
 sub checkoutModules($);
+sub updateModule($$$$);
 sub checkoutModule($$$$$$$);
+sub runCvsCommand($);
 sub modifyCvsEntries($$);
 sub expandEnvVars($);
 sub printDebug($@);
+sub nextSpinnerFrame($);
 
 # *********************************************************************
 # Here is the version for this script!
 
-my $VERSION = '0.0.7';
+my $VERSION = '0.0.10';
 # *********************************************************************
 
 my $cfg_file      = '';
@@ -66,7 +69,9 @@ my $print_version = 0;
 my $verbose       = 0;
 my $entry_mod     = 0;
 my $force_install = 0;
+my $manual        = 0;
 
+my (@limit_modules) = ();
 my (@overrides)     = ();
 my (%cmd_overrides) = ();
 
@@ -82,13 +87,24 @@ $debug_level = $CRITICAL_LVL;
 GetOptions('cfg=s' => \$cfg_file, 'help' => \$help, 'override=s' => \@overrides,
            'debug=i' => \$debug_level, 'set=s' => \%cmd_overrides,
            'version' => \$print_version, 'verbose' => \$verbose,
-           'entry-mod' => \$entry_mod, 'force-install' => \$force_install);
+           'entry-mod' => \$entry_mod, 'force-install' => \$force_install,
+           'limit=s' => \@limit_modules, 'manual' => \$manual)
+   or pod2usage(2);
 
 # Print the help output and exit if --help was on the command line.
-printHelp() && exit(0) if $help;
+pod2usage(1) if $help;
 
 # Print the version number and exit if --version was on the command line.
 printVersion() && exit(0) if $print_version;
+
+# Print the POD documentation in manpage format.  This for people who don't
+# know about perldoc (*sigh*).  The current implementation is a hack since I
+# don't know yet if there is something comparable to pod2usage().
+if ( $manual )
+{
+   system("perldoc $0");
+   exit(0);
+}
 
 # If we are doing verbose output and the user did not change the debug level,
 # then we push the debug level up to the max.
@@ -206,64 +222,32 @@ foreach $key ( keys(%cmd_overrides) )
    }
 }
 
-checkoutModules(\%override_modules);
+my (%limited_modules) = ();
+
+if ( $#limit_modules == -1 )
+{
+   %limited_modules = %override_modules;
+}
+else
+{
+   foreach ( @limit_modules )
+   {
+      if ( ! defined($override_modules{"$_"}) )
+      {
+         warn "WARNING: Trying to limit to unknown module '$_'\n";
+         next;
+      }
+
+      $limited_modules{"$_"} = $override_modules{"$_"};
+   }
+}
+
+checkoutModules(\%limited_modules);
 exit 0;
 
 # -----------------------------------------------------------------------------
 # Subroutines follow.
 # -----------------------------------------------------------------------------
-
-sub printHelp ()
-{
-   print <<EOF;
-Usage:
-    $0
-        [ --cfg=<filename> ] [ --override=<filename> ... ] [ --debug=<level> ]
-        [ --set <key>=<value> ... ] [ --entry-mod] [ --verbose ]
-
-For application makefiles:
-
-    --help
-        Print usage information.
-
-    --cfg=<filename>
-        Specify the name of the module configuration to load. If not given,
-        the current directory is searched for a .gatherrc file. If one is not
-        found, the user's home directory is searched for the same file.
-
-    --debug=<level>
-        Set the debug output level (0-5).
-
-    --override=<filename>
-        Specify the name of an an override file used to override values set
-        by the loaded module configuration file. There may be zero or more of
-        these. If not specified, the current directory is searched for the
-        file .gatherrc-override. If not found, the user's home directory is
-        searched for the same file.
-
-    --set <key1>=<value1> --set <key2>=<value2> ... --set <keyN>=<valueN>
-        Override the setting for "key" with "value". This supercedes any
-        settings in the module configuration file and any loaded override
-        file. There may be zero or more of these.
-
-    --force-install
-        When re-downloading a module, force removal of the existing
-        directory.  Without this option, a warning is printed, and the
-        existing directory is not removed.
-
-    --entry-mod
-        Modify CVS/Entries to include a newly downloaded module.  This
-        is not the default behavior.
-
-    --verbose
-        Turn on verbose output.  This basically makes the log file useless.
-
-    --version
-        Print the version number and exit.
-EOF
-
-   return 1;
-}
 
 sub printVersion ()
 {
@@ -706,20 +690,67 @@ sub checkoutModules ($)
 
          if ( defined($$mod_ref{"$mod_name"}{'CVSROOT'}) && $cvs_module_name )
          {
-            printDebug $VERB_LVL, "$mod_name --> $$mod_ref{$mod_name}{'CVSROOT'} ",
-                       "$$mod_ref{$mod_name}{'Tag'} $$mod_ref{$mod_name}{'Date'} ",
-                       "$cvs_module_name (to $$mod_ref{$mod_name}{'Path'}";
-            printDebug $VERB_LVL, "/$install_name" if $install_name;
-            printDebug $VERB_LVL, ")\n";
-            checkoutModule($mod_name, $$mod_ref{"$mod_name"}{'CVSROOT'},
-                           $$mod_ref{"$mod_name"}{'Tag'},
-                           $$mod_ref{"$mod_name"}{'Date'}, "$cvs_module_name",
-                           $$mod_ref{"$mod_name"}{'Path'}, "$install_name");
+            # If a module is already checked out, update it instead of doing
+            # a new checkout.
+            if ( ($install_name &&
+                    -d "$$mod_ref{$mod_name}{'Path'}/$install_name") ||
+                 -d "$$mod_ref{$mod_name}{'Path'}/$cvs_module_name" )
+            {
+               updateModule("$mod_name", $$mod_ref{"$mod_name"}{'Path'},
+                            "$cvs_module_name", "$install_name");
+            }
+            else
+            {
+               printDebug $VERB_LVL, "$mod_name --> ",
+                          "$$mod_ref{$mod_name}{'CVSROOT'} ",
+                          "$$mod_ref{$mod_name}{'Tag'} ",
+                          "$$mod_ref{$mod_name}{'Date'} ",
+                          "$cvs_module_name (to $$mod_ref{$mod_name}{'Path'}";
+               printDebug $VERB_LVL, "/$install_name" if $install_name;
+               printDebug $VERB_LVL, ")\n";
+
+               checkoutModule($mod_name, $$mod_ref{"$mod_name"}{'CVSROOT'},
+                              $$mod_ref{"$mod_name"}{'Tag'},
+                              $$mod_ref{"$mod_name"}{'Date'},
+                              "$cvs_module_name",
+                              $$mod_ref{"$mod_name"}{'Path'},
+                              "$install_name");
+            }
          }
       }
 
       checkoutModules($$mod_ref{"$mod_name"}{'deps'});
    }
+}
+
+sub updateModule ($$$$)
+{
+   my $name         = shift;
+   my $path         = shift;
+   my $cvs_module   = shift;
+   my $install_name = shift;
+
+   my $module = ("$install_name" ne "") ? "$install_name" : "$cvs_module";
+
+   print "Updating $name ($module) in $path ...\n";
+
+   my $cur_dir = getcwd();
+   chdir("$path") if "$path" ne "" && "$path" ne ".";
+
+   my $status;
+
+   if ( chdir("$module") )
+   {
+      $status = runCvsCommand('cvs update');
+   }
+   else
+   {
+      warn "WARNING: Update of $module in $path failed: $!\n";
+      $status =  0;
+   }
+
+   chdir("$cur_dir");
+   return $status;
 }
 
 sub checkoutModule ($$$$$$$)
@@ -762,54 +793,7 @@ sub checkoutModule ($$$$$$$)
          $temp_checkout_dir = "$cwd";
       }
 
-      # Open a pipe to read from the output of $cmd_line.
-      open(CVS_CMD, "$cmd_line 2>&1 |") or die "Can't fork: $!\n";
-      $| = 1;
-
-      # Power users will appreciate seeing the output from CVS as it happens.
-      if ( $verbose )
-      {
-         print "$_" while <CVS_CMD>;
-      }
-      # For simpler folk, we will just write out to a log file.  To keep them placated,
-      # however, there will be a little spinner that runs while CVS is doing its job.
-      else
-      {
-         my $last_char = '|';
-
-         print "$last_char";
-
-         while ( <CVS_CMD> )
-         {
-            print LOG_FILE "$_";
-
-            # Move on to the next character of the animation sequence.
-            if ( $last_char eq "|" )
-            {
-               $last_char = '/';
-            }
-            elsif ( $last_char eq "/" )
-            {
-               $last_char = '-';
-            }
-            elsif ( $last_char eq "-" )
-            {
-               $last_char = '\\';
-            }
-            elsif ( $last_char eq "\\" )
-            {
-               $last_char = '|';
-            }
-
-            # Print one status character per line of CVS output.
-            print "\b$last_char";
-         }
-      }
-
-      $| = 0;
-
-      # Close up our pipe now that we are done with it.
-      close(CVS_CMD);
+      runCvsCommand("$cmd_line");
 
       chdir("$cwd");
       mkdir("$path", 0755) unless -d "$path";
@@ -876,8 +860,8 @@ sub checkoutModule ($$$$$$$)
             rename("$orig_module_name", "$co_dir/$install_name")
                or warn "    WARNING: Module renaming failed: $!\n";
 
-            # If the user requested to do so, modify $dest_dir/CVS/Entries to include
-            # $dir.
+            # If the user requested to do so, modify $dest_dir/CVS/Entries to
+            # include $dir.
             modifyCvsEntries("$co_dir", "$install_name") if $entry_mod;
          }
       }
@@ -905,8 +889,8 @@ sub checkoutModule ($$$$$$$)
                printDebug $STATE_LVL, "Renaming $src_dir/$dir to $dest_dir/$dir\n";
                rename("$src_dir/$dir", "$dest_dir/$dir");
 
-               # If the user requested to do so, modify $dest_dir/CVS/Entries to
-               # include $dir.
+               # If the user requested to do so, modify $dest_dir/CVS/Entries
+               # to include $dir.
                modifyCvsEntries("$dest_dir", "$dir") if $entry_mod;
 
                last;
@@ -933,6 +917,45 @@ sub checkoutModule ($$$$$$$)
    {
       warn " " x $indent, "Nothing to check out for $name!\n";
    }
+}
+
+sub runCvsCommand ($)
+{
+   my $cmd_line = shift;
+
+   # Open a pipe to read from the output of $cmd_line.
+   open(CVS_CMD, "$cmd_line 2>&1 |") or die "Can't fork: $!\n";
+   $| = 1;
+
+   # Power users will appreciate seeing the output from CVS as it happens.
+   if ( $verbose )
+   {
+      print "$_" while <CVS_CMD>;
+   }
+   # For simpler folk, we will just write out to a log file.  To keep them
+   # placated, however, there will be a little spinner that runs while CVS is
+   # doing its job.
+   else
+   {
+      my $next_char = '|';
+
+      print "Working ... $next_char";
+
+      while ( <CVS_CMD> )
+      {
+         print LOG_FILE "$_";
+         $next_char = nextSpinnerFrame("$next_char");
+      }
+   }
+
+   $| = 0;
+
+   # Close up our pipe now that we are done with it.
+   close(CVS_CMD);
+
+   print "Done\n";
+
+   return 1;
 }
 
 sub modifyCvsEntries ($$)
@@ -1007,3 +1030,213 @@ sub printDebug ($@)
    my $level = shift;
    print STDERR @_ if $debug_level >= $level;
 }
+
+sub nextSpinnerFrame ($)
+{
+   my $last_frame = shift;
+
+   my $next_frame = '';
+
+   # Move on to the next character of the animation sequence.
+   if ( $last_frame eq "|" )
+   {
+      $next_frame = '/';
+   }
+   elsif ( $last_frame eq "/" )
+   {
+      $next_frame = '-';
+   }
+   elsif ( $last_frame eq "-" )
+   {
+      $next_frame = '\\';
+   }
+   elsif ( $last_frame eq "\\" )
+   {
+      $next_frame = '|';
+   }
+
+   # Print one status character per line of CVS output.
+   print "\b$next_frame";
+
+   return "$next_frame";
+}
+
+__END__
+
+=head1 NAME
+
+cvs-gather.pl
+
+=head1 SYNOPSYS
+
+This script gathers together CVS modules from one or more repositories
+into a single development tree.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help>
+
+Print usage information.
+
+=item B<--cfg=<filename>>
+
+Specify the name of the module configuration to load. If not given,
+the current directory is searched for a .gatherrc file. If one is not
+found, the user's home directory is searched for the same file.
+
+=item B<--debug=<level>>
+
+Set the debug output level (0-5).
+
+=item B<--entry-mod>
+
+Modify CVS/Entries to include a newly downloaded module.  This
+is I<not> the default behavior.
+
+=item B<--force-install>
+
+When re-downloading a module, force removal of the existing
+directory.  Without this option, a warning is printed, and the
+existing directory is not removed.
+
+=item B<--limit <module1>> B<--limit <module2>> ... B<--limit <moduleN>>
+
+=item B<--limit <module1,module2,...,moduleN>>
+
+Limit the gathered modules to those listed.  The list may be defined by
+multiple B<--limit> arguments or by specifying a single B<--limit>
+argument with a comma-separated list of module names.  The named module
+may only be a "top-level" module, and all dependencies of the named module
+will be gathered.  In other words, the named module cannot exist solely as
+a dependency of an enclosing module.
+
+=item B<--manual>
+
+Print the documentation for this script in manpage format.  This has
+exactly the same effect as invoking perldoc(1) on this script.
+
+=item B<--override=<filename>>
+
+Specify the name of an an override file used to override values set
+by the loaded module configuration file. There may be zero or more of
+these. If not specified, the current directory is searched for the
+file .gatherrc-override. If not found, the user's home directory is
+searched for the same file.
+
+=item B<--set> <key1>=<value1> --set <key2>=<value2> ... --set <keyN>=<valueN>
+
+Override the setting for "key" with "value". This supercedes any
+settings in the module configuration file and any loaded override
+file. There may be zero or more of these.  The key is a dotted string
+containing zero or more wildcards that describes the hierarchy of a
+given element (Project.Dep1.CVSROOT, for example).  The value is the
+new value for the named key.  Wildcards can only appear in place of
+module names.
+
+=item B<--verbose>
+
+Turn on verbose output.  This basically makes the log file useless.
+
+=item B<--version>
+
+Print the version number and exit.
+
+=back
+
+=head1 FILE FORMATS
+
+This script handles two different types of files: basic configuration
+files and configuration override files.  The former describes what
+will be downloaded, and the latter provides a convenient mechanism for
+overriding settings in the basic configuration.  Thus, a single
+configuration file can be used by multiple users, each with their own
+custom overrides.
+
+=head2 Basic configuration
+
+The basic configuration file format is comparable to a C/C++ struct.
+It defines a hierarchical grouping of data.  The hierarchies are made
+up of projects, their dependencies, dependencies of the dependencies,
+and so on.  A single configuration file can contain multiple projects,
+each with their own dependencies.  Information can be shared by including
+external files at any point throughout the file.
+
+Within each project, several properties may be set that define the CVS
+settings needed to download the project.  These include the repository
+root, the repository module name, a tag on the code, a date for the
+code, and a path to place the checked out code.  Of these, none are
+required.  This allows a "global" module that defines something similar
+to a C++ namespace.  However, if a repository root is named, a module
+within that repository must be named too.
+
+=head2 Configuration overrides
+
+To override settings provided through a basic configuration file, the
+user may choose to use the override capabilities.  The overrides may be
+specified several ways, one of which is through the use of a file.  The
+format of the file is very simple: one line per override with each line
+taking the form:
+
+key = value
+
+Here, B<key> is some dotted string that describes the module hierarchy, and
+B<value> is the new value for that key.  Consider the following example
+configuration:
+
+My_Module
+{
+   CVSROOT: :pserver:anon@server.net:/cvsroot/mymod;
+   Module: mine;
+
+   Dep1
+   {
+      CVSROOT: user@cvs-server2.com:/cvsroot/DepRepos;
+      Module: mathlib;
+   }
+};
+
+To override the CVSROOT of Dep1, the following could be done:
+
+My_Module.Dep1.CVSROOT = :pserver:anon@cvs-server2.com:/cvsroot/DepRepos
+
+Several modules can have a common property overridden using the wildcard
+character B<*>.  For example:
+
+My_Module.*.CVSROOT = :pserver:anon@cvs-server2.com:/cvsroot/DepRepos
+
+This would override the CVSROOT settings for I<all> direct dependencies
+of My_Module.  (Dependencies of those dependencies will not be affected,
+however).  The wildcard character may only be specified for module names.
+It would not make sense to set the CVSROOT, Tag, and Date properties to
+the same value.
+
+Comments in this file begin with the B<#> (hash) character and extend to
+the end of the line.  No other comment syntax is supported.
+
+=head1 FILES
+
+=over 8
+
+=item B<./.gatherrc>, B<$HOME/.gatherrc>
+
+If no configuration file is specified using B<--cfg>, the script will
+search the current directory for the file B<.gatherrc>.  If not found,
+the user's home directory is searched for a file of the same name.
+This is the basic configuration file that names the module (or modules)
+and any dependencies.  A configuration file must be specified using one
+of these three methods for the script to do any work.
+
+=item B<./.gatherrc-override>, B<$HOME/.gatherrc-override>
+
+After loading the configuration file, values set in the configuration
+may be overridden.  An override file may be specified using B<--override>,
+or individual settings may be overridden using B<--set>.  In addition,
+the current directory is searched for a file named B<.gatherrc-override>.
+If not found, the user's home directory is searched for a file of the
+same name.  Override settings are optional.
+
+=back
+
+=out
