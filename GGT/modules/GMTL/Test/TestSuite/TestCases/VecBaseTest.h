39deab1ef40eda50759ff02124bbca173028dfef
nonchocoboy <nonchocoboy@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-03-18 23:09:12 +0000

    Split up the VecBase test case.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@349 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/Test/TestSuite/TestCases/VecBaseTest.h
+++ GGT/modules/GMTL/Test/TestSuite/TestCases/VecBaseTest.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: VecBaseTest.h,v $
- * Date modified: $Date: 2002-03-18 18:25:14 $
- * Version:       $Revision: 1.5 $
+ * Date modified: $Date: 2002-03-18 23:09:12 $
+ * Version:       $Revision: 1.6 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -33,13 +33,10 @@
 *
  ************************************************************ ggt-cpr end */
 #include <gmtl/gmtlConfig.h>
-#include <iostream>
 
 #include <cppunit/TestCase.h>
 #include <cppunit/TestSuite.h>
 #include <cppunit/TestCaller.h>
-
-#include <gmtl/VecBase.h>
 
 namespace gmtlTest
 {
@@ -61,238 +58,13 @@ public:
    {
    }
 
-   void testVecBaseCreation()
-   {
-      gmtl::VecBase<double, 3> vec;
-      vec[0] = 0.0f; // use the var
+   void testVecBaseCreation();
+   void testCopyConstruct();
+   void testConstructors();
 
-      /*
-      CPPUNIT_ASSERT( vec[0] == 0.0f);
-      CPPUNIT_ASSERT( vec[1] == 0.0f);
-      CPPUNIT_ASSERT( vec[2] == 0.0f);
-      */
-
-      // Test overhead of creation
-      const long iters(400000);
-      float use_value(0);
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::VecBase<float, 2> test_vec2;
-         test_vec2[0] = 1.0f;
-         gmtl::VecBase<float, 3> test_vec3;
-         test_vec3[0] = 2.0f;
-         gmtl::VecBase<float, 4> test_vec4;
-         test_vec4[0] = 3.0f;
-
-         use_value = use_value + test_vec2[0] + test_vec3[0] + test_vec4[0];
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("VecBaseTest/VecCreationOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-
-      CPPUNIT_ASSERT( use_value > 0.0f );
-   }
-
-   void testCopyConstruct()
-   {
-      gmtl::VecBase<double, 3> test_vec;
-
-      test_vec[0] = 2.0f;
-      test_vec[1] = 4.0f;
-      test_vec[2] = 8.0f;
-
-      gmtl::VecBase<double, 3> test_vec_copy(test_vec);
-
-      CPPUNIT_ASSERT( test_vec_copy[0] == 2.0f);
-      CPPUNIT_ASSERT( test_vec_copy[1] == 4.0f);
-      CPPUNIT_ASSERT( test_vec_copy[2] == 8.0f);
-
-      // Test copy construction overhead
-      const long iters(400000);
-      gmtl::VecBase<float, 2> test_vec2;
-      test_vec2[0] = 2.0f;
-      gmtl::VecBase<float, 3> test_vec3;
-      test_vec2[0] = 3.0f;
-      gmtl::VecBase<float, 4> test_vec4;
-      test_vec2[0] = 4.0f;
-      float use_value;     // A temp just here to use the objs so the copiler (hopefully) does not opt them out
-
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::VecBase<float, 2> test_vec2_copy(test_vec2);
-         use_value += test_vec2_copy[0];
-         gmtl::VecBase<float, 3> test_vec3_copy(test_vec3);
-         use_value += test_vec3_copy[0];
-         gmtl::VecBase<float, 4> test_vec4_copy(test_vec4);
-         use_value += test_vec4_copy[0];
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("VecBaseTest/CopyConstructOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-
-   void testConstructors()
-   {
-      gmtl::VecBase<float, 4> test_vec4(1.0f, 2.0f, 3.0f, 4.0f);
-      CPPUNIT_ASSERT( test_vec4[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec4[1] == 2.0f);
-      CPPUNIT_ASSERT( test_vec4[2] == 3.0f);
-      CPPUNIT_ASSERT( test_vec4[3] == 4.0f);
-
-      gmtl::VecBase<float, 3> test_vec3(1.0f, 2.0f, 3.0f);
-      CPPUNIT_ASSERT( test_vec3[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec3[1] == 2.0f);
-      CPPUNIT_ASSERT( test_vec3[2] == 3.0f);
-
-      gmtl::VecBase<float, 2> test_vec2(1.0f, 2.0f);
-      CPPUNIT_ASSERT( test_vec2[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec2[1] == 2.0f);
-
-      gmtl::VecBase<float, 1> test_vec1(1.0f);
-      CPPUNIT_ASSERT( test_vec1[0] == 1.0f);
-
-      // Test constructor
-      const long iters(400000);
-      float use_value(0.0f);     // A temp just here to use the objs so the copiler (hopefully) does not opt them out
-
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::VecBase<float, 4> test_vec4(1.0f, 2.0f, 3.0f, 4.0f);
-         gmtl::VecBase<float, 3> test_vec3(1.0f, 2.0f, 3.0f);
-         gmtl::VecBase<float, 2> test_vec2(1.0f, 2.0f);
-         gmtl::VecBase<float, 1> test_vec1(1.0f);
-
-         use_value = use_value + test_vec4[3] + test_vec3[2] + test_vec2[1] + test_vec1[0];
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("VecBaseTest/ConstructorsOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testSet()
-   {
-      gmtl::VecBase<float, 4> test_vec4;
-      test_vec4.set(1.0f, 2.0f, 3.0f, 4.0f);
-      CPPUNIT_ASSERT( test_vec4[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec4[1] == 2.0f);
-      CPPUNIT_ASSERT( test_vec4[2] == 3.0f);
-      CPPUNIT_ASSERT( test_vec4[3] == 4.0f);
-
-      gmtl::VecBase<float, 3> test_vec3;
-      test_vec3.set(1.0f, 2.0f, 3.0f);
-      CPPUNIT_ASSERT( test_vec3[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec3[1] == 2.0f);
-      CPPUNIT_ASSERT( test_vec3[2] == 3.0f);
-
-      gmtl::VecBase<float, 2> test_vec2;
-      test_vec2.set(1.0f, 2.0f);
-      CPPUNIT_ASSERT( test_vec2[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec2[1] == 2.0f);
-
-      gmtl::VecBase<float, 1> test_vec1;
-      test_vec1.set(1.0f);
-      CPPUNIT_ASSERT( test_vec1[0] == 1.0f);
-
-      // Test constructor
-      const float iters(400000);
-      float use_value(0.0f);     // A temp just here to use the objs so the copiler (hopefully) does not opt them out
-
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( float iter=0;iter<iters; ++iter)
-      {
-         test_vec4.set(iters+0, iters+1, iters+2, iters+3);
-         test_vec3.set(iters+0, iters+1, iters+2);
-         test_vec2.set(iters+0, iters+1);
-         test_vec1.set(iters+0);
-
-         use_value = use_value + test_vec4[3] + test_vec3[2] + test_vec2[1] + test_vec1[0];
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("VecBaseTest/SetOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-
-      CPPUNIT_ASSERT( use_value > 0.0f );
-   }
-
-   void testSetPtr()
-   {
-      float data[4] = {1.0f, 2.0f, 3.0f, 4.0f};
-
-      gmtl::VecBase<float, 4> test_vec4;
-      test_vec4.set(data);
-      CPPUNIT_ASSERT( test_vec4[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec4[1] == 2.0f);
-      CPPUNIT_ASSERT( test_vec4[2] == 3.0f);
-      CPPUNIT_ASSERT( test_vec4[3] == 4.0f);
-
-      gmtl::VecBase<float, 3> test_vec3;
-      test_vec3.set(data);
-      CPPUNIT_ASSERT( test_vec3[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec3[1] == 2.0f);
-      CPPUNIT_ASSERT( test_vec3[2] == 3.0f);
-
-      gmtl::VecBase<float, 2> test_vec2;
-      test_vec2.set(data);
-      CPPUNIT_ASSERT( test_vec2[0] == 1.0f);
-      CPPUNIT_ASSERT( test_vec2[1] == 2.0f);
-
-      gmtl::VecBase<float, 1> test_vec1;
-      test_vec1.set(data);
-      CPPUNIT_ASSERT( test_vec1[0] == 1.0f);
-
-      // Test constructor
-      const float iters(400000);
-      float use_value(0.0f);     // A temp just here to use the objs so the copiler (hopefully) does not opt them out
-
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( float iter=0;iter<iters; ++iter)
-      {
-         data[0] += 1.0f; data[1] += 2.0f; data[2] += 4.0f; data[3] += 8.0f;    // Just make sure data changes
-         test_vec4.set(data);
-         test_vec3.set(data);
-         test_vec2.set(data);
-         test_vec1.set(data);
-
-         use_value = use_value + test_vec4[3] + test_vec3[2] + test_vec2[1] + test_vec1[0];
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("VecBaseTest/SetPtrOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-
-      CPPUNIT_ASSERT( use_value > 0.0f );
-   }
-
-
-   void testGetData()
-   {
-      float* data;
-      gmtl::VecBase<float, 4> test_vec4(1.0f, 2.0f, 3.0f, 4.0f);
-      data = test_vec4.getData();
-      CPPUNIT_ASSERT( data[0] == 1.0f);
-      CPPUNIT_ASSERT( data[1] == 2.0f);
-      CPPUNIT_ASSERT( data[2] == 3.0f);
-      CPPUNIT_ASSERT( data[3] == 4.0f);
-
-      gmtl::VecBase<float, 3> test_vec3(1.0f, 2.0f, 3.0f);
-      data = test_vec3.getData();
-      CPPUNIT_ASSERT( data[0] == 1.0f);
-      CPPUNIT_ASSERT( data[1] == 2.0f);
-      CPPUNIT_ASSERT( data[2] == 3.0f);
-
-      gmtl::VecBase<float, 2> test_vec2(1.0f, 2.0f);
-      data = test_vec2.getData();
-      CPPUNIT_ASSERT( data[0] == 1.0f);
-      CPPUNIT_ASSERT( data[1] == 2.0f);
-   }
+   void testSet();
+   void testSetPtr();
+   void testGetData();
 
    static CppUnit::Test* suite()
    {
