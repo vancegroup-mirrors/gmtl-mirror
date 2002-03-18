e1fa46ff8edb50492cf8509e74114b3c97e83fb6
nonchocoboy <nonchocoboy@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-03-18 22:11:14 +0000

    Split up the Plane test case.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@341 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- Test/TestSuite/TestCases/PlaneTest.h
+++ Test/TestSuite/TestCases/PlaneTest.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: PlaneTest.h,v $
- * Date modified: $Date: 2002-03-18 18:25:13 $
- * Version:       $Revision: 1.16 $
+ * Date modified: $Date: 2002-03-18 22:11:14 $
+ * Version:       $Revision: 1.17 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -40,7 +40,6 @@
 #include <cppunit/TestCaller.h>
 
 #include <gmtl/Plane.h>
-#include <gmtl/PlaneOps.h>
 
 namespace gmtlTest
 {
@@ -75,356 +74,25 @@ public:
    // ------------------------
    // CREATION TESTS
    // ------------------------
-   void testCreation()
-   {
-      gmtl::Plane<float> test_plane;
-      gmtl::Vec<float, 3> zeroVec(0,0,0);
-      CPPUNIT_ASSERT( test_plane.mNorm == zeroVec );
-      CPPUNIT_ASSERT( test_plane.mOffset == 0.0f );
-
-      // Test overhead of creation
-      const long iters(400000);
-      float use_value(0);
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::Plane<float> test_plane2;
-         use_value += test_plane2.mOffset + 1.0f;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/DefaultCreationOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-
-      CPPUNIT_ASSERT( use_value > 0.0f );
-   }
-
-   void testThreePtCreation()
-   {
-      // Check normals of the standard planes
-      CPPUNIT_ASSERT(xy_plane.mNorm == z1_v);    // Z-Plane
-      CPPUNIT_ASSERT(zx_plane.mNorm == y1_v);    // Y-Plane
-      CPPUNIT_ASSERT(yz_plane.mNorm == x1_v);    // Z-Plane
-      CPPUNIT_ASSERT(xy_plane.mOffset == 0.0f);
-      CPPUNIT_ASSERT(zx_plane.mOffset == 0.0f);
-      CPPUNIT_ASSERT(yz_plane.mOffset == 0.0f);
-
-      // Test offset of some simple planes
-      gmtl::Plane<float> test_plane;
-      test_plane = gmtl::Plane<float>(
-           gmtl::Point<float, 3>(1.0,0.0,0.0),
-           gmtl::Point<float, 3>(1.0,1.0,0.0),
-           gmtl::Point<float, 3>(1.0,0.0,1.0)
-      );
-      CPPUNIT_ASSERT(test_plane.mNorm == x1_v);
-      CPPUNIT_ASSERT(test_plane.mOffset == 1.0f);
-
-      // Test overhead of creation
-      const long iters(400000);
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::Plane<float> test_plane( x1_pt, y1_pt, z1_pt );
-         test_plane.mOffset = 1.0f;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/ThreePtCreationOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testNormPtCreation()
-   {
-      gmtl::Plane<float> test_plane;
-      test_plane = gmtl::Plane<float>(x1_v, origin);    // X-axis through origin
-      CPPUNIT_ASSERT(test_plane.mNorm == x1_v);
-      CPPUNIT_ASSERT(test_plane.mOffset == 0.0f);
-
-      test_plane = gmtl::Plane<float>(x1_v, x1_pt);    // X-axis through 1,0,0
-      CPPUNIT_ASSERT(test_plane.mNorm == x1_v);
-      CPPUNIT_ASSERT(test_plane.mOffset == 1.0f);
-
-      test_plane = gmtl::Plane<float>(z1_v, x1_pt);    // Z-axis through 1,0,0
-      CPPUNIT_ASSERT(test_plane.mNorm == z1_v);
-      CPPUNIT_ASSERT(test_plane.mOffset == 0.0f);
-
-      test_plane = gmtl::Plane<float>(z1_v, gmtl::Point<float, 3>(0,0,-1));    // Z-axis through 0,0,-1
-      CPPUNIT_ASSERT(test_plane.mNorm == z1_v);
-      CPPUNIT_ASSERT(test_plane.mOffset == -1.0f);
-
-      // Test overhead of creation
-      const long iters(400000);
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::Plane<float> test_plane2( x1_v, z1_pt );
-         test_plane2.mOffset = 1.0f;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/NormPtCreationOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testNormOffsetCreation()
-   {
-      gmtl::Plane<float> test_plane(x1_v, 0.0f);    // X-axis through origin
-      CPPUNIT_ASSERT(test_plane.mNorm == x1_v);
-      CPPUNIT_ASSERT(test_plane.mOffset == 0.0f);
-
-      // Test overhead of creation
-      const long iters(400000);
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::Plane<float> test_plane2( x1_v, 25.0f );
-         test_plane2.mOffset = 1.0f;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/NormOffsetCreationOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testCopyConstruct()
-   {
-      gmtl::Plane<float> test_plane( xy_plane );
-      CPPUNIT_ASSERT( test_plane.mNorm == xy_plane.mNorm );
-      CPPUNIT_ASSERT( test_plane.mOffset == xy_plane.mOffset );
-
-      // Test overhead of creation
-      const long iters(400000);
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         gmtl::Plane<float> test_plane2( test_plane );
-         test_plane2.mOffset = 1.0f;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/CopyConstructOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
+   void testCreation();
+   void testThreePtCreation();
+   void testNormPtCreation();
+   void testNormOffsetCreation();
+   void testCopyConstruct();
 
    // --------------------------
    // Comparison tests
    // --------------------------
-   void testEqualityCompare()
-   {
-      gmtl::Plane<float> test_plane1( x1_v, 35.0f );
-      gmtl::Plane<float> test_plane2( test_plane1 );
-
-      CPPUNIT_ASSERT( test_plane1 == test_plane2 );
-      CPPUNIT_ASSERT( !(test_plane1 != test_plane2) );
-
-      // set equal, vary normal
-      test_plane2 = test_plane1;
-      test_plane2.mNorm[0] += 2.0f;
-      CPPUNIT_ASSERT( test_plane1 != test_plane2 );
-      CPPUNIT_ASSERT( !(test_plane1 == test_plane2) );
-
-      // set equal, vary offset
-      test_plane2 = test_plane1;
-      test_plane2.mOffset += 2.0f;
-      CPPUNIT_ASSERT( test_plane1 != test_plane2 );
-      CPPUNIT_ASSERT( !(test_plane1 == test_plane2) );
-
-      // test comparison performance
-      const long iters(400000);
-      unsigned true_count(0);
-
-      // -- Equality
-      test_plane2 = test_plane1;
-
-      CPPUNIT_METRIC_START_TIMING();
-      for( long iter=0; iter<iters; ++iter )
-      {
-         test_plane1.mOffset += 1.0f;
-         test_plane2.mOffset += 2.0f;
-         if ( test_plane1 == test_plane2 )
-            true_count++;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/EqualityCompareOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-
-      // -- Inequality
-      test_plane1.mNorm = x1_v;
-      test_plane1.mOffset = 0.0f;
-      test_plane2 = test_plane1;
-
-      CPPUNIT_METRIC_START_TIMING();
-      for( long iter=0; iter<iters; ++iter )
-      {
-         test_plane1.mOffset += 1.0f;
-         test_plane2.mOffset += 2.0f;
-         if ( test_plane1 == test_plane2 )
-            true_count++;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/InequalityCompareOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testIsEqual()
-   {
-      gmtl::Plane<float> test_plane1( x1_v, 0.0f );
-      gmtl::Plane<float> test_plane2( test_plane1 );
-      float eps(0.0f);
-
-      for( eps=0.0f; eps<10.0f; eps+=0.05f )
-      {
-         CPPUNIT_ASSERT( gmtl::isEqual(test_plane1, test_plane2, eps) );
-      }
-
-      for ( unsigned elt=0; elt<4; ++elt )
-      {
-         test_plane2 = test_plane1;
-         if ( elt < 3 ) {
-            test_plane2.mNorm[elt] += 20.0f;
-         } else {
-            test_plane2.mOffset += 20.0f;
-         }
-         CPPUNIT_ASSERT( !gmtl::isEqual(test_plane1, test_plane2, 10.0f) );
-         CPPUNIT_ASSERT( !gmtl::isEqual(test_plane1, test_plane2, 19.9f) );
-         CPPUNIT_ASSERT( gmtl::isEqual(test_plane1, test_plane2, 20.1f) );
-         CPPUNIT_ASSERT( gmtl::isEqual(test_plane1, test_plane2, 22.0f) );
-      }
-
-      // Test comparison performance
-      const long iters(400000);
-      unsigned true_count(0);
-
-      // -- Equality
-      CPPUNIT_METRIC_START_TIMING();
-      test_plane2 = test_plane1;
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         test_plane1.mOffset += 1.0f;
-         test_plane2.mOffset += 2.0f;
-         if(gmtl::isEqual(test_plane1, test_plane2, 1.0f) )
-            true_count++;
-         if(gmtl::isEqual(test_plane1, test_plane2, 0.1f) )
-            true_count++;
-         if(gmtl::isEqual(test_plane1, test_plane2, 100000.0f) )
-            true_count++;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/isEqualOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
+   void testEqualityCompare();
+   void testIsEqual();
 
    // --------------------------
    // Operations tests
    // --------------------------
-   void testDistance()
-   {
-      gmtl::Plane<float> test_plane( x1_v, 25.0f );
-      CPPUNIT_ASSERT( gmtl::distance(test_plane, origin) == -25.0f );
-      CPPUNIT_ASSERT( gmtl::distance(test_plane, x1_pt) == -24.0f );
+   void testDistance();
+   void testWhichSide();
+   void testFindNearestPt();
 
-      CPPUNIT_ASSERT( gmtl::distance(xy_plane, z1_pt) == 1.0f );
-
-      gmtl::Point<float, 3> pt(-12.0f, 5.0f, -17.0f);
-      CPPUNIT_ASSERT( gmtl::distance(xy_plane, pt) == -17.0f );
-
-      gmtl::Plane<float> slanted_plane( gmtl::Vec<float, 3>(1.0f,1.0f,1.0f), origin );
-      CPPUNIT_ASSERT( gmtl::distance(slanted_plane, origin) == 0.0f );
-
-      pt.set(1.0f, 1.0f, 1.0f );
-      CPPUNIT_ASSERT( gmtl::distance(slanted_plane, pt) > 0.0f ); 
-
-      // Test distance performance
-      const long iters(400000);
-      float use_value(0.0f);
-
-      // -- Equality
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0;iter<iters; ++iter)
-      {
-         test_plane.mOffset += 1.0f;
-         use_value = use_value + gmtl::distance(test_plane, y1_pt);
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/DistanceOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testWhichSide()
-   {
-      gmtl::PlaneSide answer;
-
-      answer = gmtl::whichSide(xy_plane, gmtl::Point<float, 3>(0.0,0.0,1.0));
-      CPPUNIT_ASSERT( answer == gmtl::POS_SIDE );
-
-      answer = gmtl::whichSide(xy_plane, gmtl::Point<float, 3>(0.0,0.0,-12.0f));
-      CPPUNIT_ASSERT( answer == gmtl::NEG_SIDE );
-
-      answer = gmtl::whichSide(zx_plane, gmtl::Point<float, 3>(0.0,1e-10f,0.0f));
-      CPPUNIT_ASSERT( answer == gmtl::POS_SIDE );
-
-      answer = gmtl::whichSide(zx_plane, gmtl::Point<float, 3>(0.0,-1e-10f,0.0f));
-      CPPUNIT_ASSERT( answer == gmtl::NEG_SIDE );
-
-      answer = gmtl::whichSide(xy_plane, gmtl::Point<float, 3>(0.0,0.0,0.0));
-      CPPUNIT_ASSERT( answer == gmtl::ON_PLANE );
-
-      // Test whichSide performance
-      const long iters(400000);
-      unsigned true_count(0);
-
-      // -- Equality
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0; iter<iters; ++iter)
-      {
-         if ( gmtl::distance(xy_plane, y1_pt) == gmtl::POS_SIDE )
-            ++true_count;
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/WhichSideOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
-
-   void testFindNearestPt()
-   {
-      gmtl::Point<float, 3> answer, test_point, correct_result;
-      float dist;
-
-      // XY dist to point off origin
-      test_point = gmtl::Point<float, 3>(0.0,0.0,1.0);
-      CPPUNIT_ASSERT(gmtl::distance(xy_plane, test_point) == gmtl::findNearestPt(xy_plane, test_point, answer));
-      CPPUNIT_ASSERT(answer == origin);
-
-      // XY dist to point at 12,21
-      test_point = gmtl::Point<float, 3>(12.0,-21.0,-13.0);
-      correct_result = gmtl::Point<float, 3>(12.0,-21.0,0.0);
-      CPPUNIT_ASSERT(gmtl::distance(xy_plane, test_point) == gmtl::findNearestPt(xy_plane, test_point, answer));
-      CPPUNIT_ASSERT(answer == correct_result);
-
-      // XY dist to point on plane at -17.05, 0.334
-      test_point = gmtl::Point<float, 3>(-17.05f,0.334f,0.0f);
-      CPPUNIT_ASSERT(gmtl::distance(xy_plane, test_point) == gmtl::findNearestPt(xy_plane, test_point, answer));
-      CPPUNIT_ASSERT(answer == test_point);
-
-      // Test findNearestPt performance
-      const long iters(400000);
-      float use_value(0.0f);
-
-      // -- Equality
-      CPPUNIT_METRIC_START_TIMING();
-
-      for( long iter=0; iter<iters; ++iter)
-      {
-         dist = gmtl::findNearestPt(xy_plane, test_point, answer);
-         use_value = use_value + dist + answer[0];
-      }
-
-      CPPUNIT_METRIC_STOP_TIMING();
-      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/FindNearestPtOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
-   }
 
    static CppUnit::Test* suite()
    {
