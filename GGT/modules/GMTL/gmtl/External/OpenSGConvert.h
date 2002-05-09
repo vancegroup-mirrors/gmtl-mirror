204e789840e65bc462abaf4803d9e204f18746f6
allenb <allenb@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-05-09 16:31:09 +0000

    Added helper methods for converting between gmtl and opensg.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@463 6382ae23-ed94-47d5-a97d-470f0bc4535f

---
+++ GGT/modules/GMTL/gmtl/External/OpenSGConvert.h
@@ -0,0 +1,41 @@
+#ifndef GMTL_OPENSG_CONVERT_H_
+#define GMTL_OPENSG_CONVERT_H_
+
+/** @file GMTL to OpenSG conversion functions
+* 
+* methods to convert between gtml and opensg matrix classes
+*/
+
+#include <gmtl/Matrix.h>
+#include <gmtl/Generate.h>
+#include <OpenSG/OSGMatrix.h>
+
+namespace gmtl
+{
+
+template <typename MATRIX_TYPE>
+inline MATRIX_TYPE makeMatrix( osg::Matrix osg_mat, Type2Type< MATRIX_TYPE > t = Type2Type< MATRIX_TYPE >() )
+{
+   MATRIX_TYPE temporary;
+   return convert( temporary, osg_mat );
+}
+
+/** Convert an opensg matrix to a gmtl::Matrix
+*/
+Matrix44f& convert( Matrix44f& mat, const osg::Matrix& osg_mat )
+{
+   mat.set(osg_mat.getValues());
+   return mat;
+}
+
+osg::Matrix& convert(osg::Matrix& osg_mat, const Matrix44f& mat)
+{
+   osg_mat.setValue( mat.getData() );
+   return osg_mat;
+}
+
+
+}
+
+#endif
+
