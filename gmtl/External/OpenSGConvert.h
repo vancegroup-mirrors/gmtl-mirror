08c6cf0514fe5b11d7a81a6bc56ade0d3502c24f
allenb <allenb@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-05-09 19:22:18 +0000

    Inline the convert functions.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@468 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- gmtl/External/OpenSGConvert.h
+++ gmtl/External/OpenSGConvert.h
@@ -15,12 +15,14 @@ namespace gmtl
 
 /** Convert an opensg matrix to a gmtl::Matrix
 */
+inline
 Matrix44f& convert( Matrix44f& mat, const osg::Matrix& osg_mat )
 {
    mat.set(osg_mat.getValues());
    return mat;
 }
 
+inline
 osg::Matrix& convert(osg::Matrix& osg_mat, const Matrix44f& mat)
 {
    osg_mat.setValue( mat.getData() );
