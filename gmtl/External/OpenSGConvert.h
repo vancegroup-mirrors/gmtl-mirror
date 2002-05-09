c9f74f9de4650f72d1f8febd9ed4930191c51b58
allenb <allenb@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-05-09 19:19:32 +0000

    Generalize the makeMatrix template for external matrix types.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@467 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- gmtl/External/OpenSGConvert.h
+++ gmtl/External/OpenSGConvert.h
@@ -13,21 +13,6 @@
 namespace gmtl
 {
 
-template <typename MATRIX_TYPE>
-inline MATRIX_TYPE makeMatrix( osg::Matrix osg_mat, Type2Type< MATRIX_TYPE > t = Type2Type< MATRIX_TYPE >() )
-{
-   MATRIX_TYPE temporary;
-   return convert( temporary, osg_mat );
-}
-
-template <>
-inline osg::Matrix makeMatrix<osg::Matrix>( gmtl::Matrix44f mat, Type2Type< osg::Matrix > t = Type2Type< osg::Matrix >() )
-{
-   osg::Matrix temp_osg_mat;
-   return convert( temp_osg_mat, mat );
-}
-
-
 /** Convert an opensg matrix to a gmtl::Matrix
 */
 Matrix44f& convert( Matrix44f& mat, const osg::Matrix& osg_mat )
@@ -41,7 +26,6 @@ osg::Matrix& convert(osg::Matrix& osg_mat, const Matrix44f& mat)
    osg_mat.setValue( mat.getData() );
    return osg_mat;
 }
-
 
 }
 
