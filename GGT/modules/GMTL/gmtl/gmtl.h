ec46197224f9af893a39aeb5194cc5511e352c41
subatomic <subatomic@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-05-20 21:56:12 +0000

    reduced gmtlConfig.  much unneeded stuff there. added gmtl.h  user friendly name to include the basics.  currently the user still has to include type specific files.  this just includes math and assert...
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@488 6382ae23-ed94-47d5-a97d-470f0bc4535f

---
+++ GGT/modules/GMTL/gmtl/gmtl.h
@@ -0,0 +1,11 @@
+#ifndef GMTL_HEADERS
+#define GMTL_HEADERS
+
+// -- Common GMTL classes -- //
+#include <gmtl/gmtlConfig.h>
+#include <gmtl/gmtlTypes.h>
+#include <gmtl/gmtlDefines.h>
+#include <gmtl/Math.h>
+#include <gmtl/Assert.h>
+
+#endif
