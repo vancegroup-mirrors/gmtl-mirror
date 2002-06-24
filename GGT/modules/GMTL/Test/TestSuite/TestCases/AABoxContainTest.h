7be2f43911b7e0536d896ff2c8665d28db10d7f3
nonchocoboy <nonchocoboy@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-06-24 03:35:07 +0000

    Added isInVolume for AABox.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@596 6382ae23-ed94-47d5-a97d-470f0bc4535f

---
+++ GGT/modules/GMTL/Test/TestSuite/TestCases/AABoxContainTest.h
@@ -0,0 +1,99 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: AABoxContainTest.h,v $
+ * Date modified: $Date: 2002-06-24 03:35:07 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
+#ifndef _GMTL_AABOXCONTAINTEST_H_
+#define _GMTL_AABOXCONTAINTEST_H_
+#include <iostream>
+
+#include <cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCaller.h>
+
+namespace gmtlTest
+{
+
+class AABoxContainTest : public CppUnit::TestCase
+{
+public:
+   AABoxContainTest( std::string name = "AABoxContainTest" )
+      : CppUnit::TestCase (name)
+   {}
+
+   //---------------------------------------------------------------------------
+   // Functionality tests
+   //---------------------------------------------------------------------------
+   void testIsInVolumePt();
+   void testIsInVolumeAABox();
+
+   //---------------------------------------------------------------------------
+   // Performance tests
+   //---------------------------------------------------------------------------
+   void testTimingIsInVolumePt();
+   void testTimingIsInVolumeAABox();
+
+
+   static CppUnit::Test* suite()
+   {
+      CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("AABoxContainTest");
+#define ADD_TEST(x) test_suite->addTest(new CppUnit::TestCaller<AABoxContainTest>( "test" #x, &AABoxContainTest::test ## x))
+      ADD_TEST(IsInVolumePt);
+      ADD_TEST(IsInVolumeAABox);
+#undef ADD_TEST
+      return test_suite;
+   }
+
+   static CppUnit::Test* perfSuite()
+   {
+      CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("AABoxContainPerfTest");
+#define ADD_TEST(x) test_suite->addTest(new CppUnit::TestCaller<AABoxContainTest>( "testTiming" #x, &AABoxContainTest::testTiming ## x))
+      ADD_TEST(IsInVolumePt);
+      ADD_TEST(IsInVolumeAABox);
+#undef ADD_TEST
+      return test_suite;
+   }
+
+   static CppUnit::Test* interactiveSuite()
+   {
+      CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("InteractiveAABoxTest");
+      //test_suite->addTest( new CppUnit::TestCaller<ThreadTest>("interactiveCPUGrind", &ThreadTest::interactiveTestCPUGrind));
+      return test_suite;
+   }
+
+protected:
+
+};
+
+}
+
+#endif
