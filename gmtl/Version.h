48415e476cf0cff21c18bb728a715efd4a1821c5
subatomic <subatomic@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-08-06 21:12:16 +0000

    added note about new intersect func.  bumped version to 0.1.8
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@630 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- gmtl/Version.h
+++ gmtl/Version.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Version.h,v $
- * Date modified: $Date: 2002-07-29 00:04:49 $
- * Version:       $Revision: 1.8 $
+ * Date modified: $Date: 2002-08-06 21:12:16 $
+ * Version:       $Revision: 1.9 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -51,7 +51,7 @@
 // The major/minor/patch version (up to 3 digits each).
 #define GMTL_VERSION_MAJOR    0
 #define GMTL_VERSION_MINOR    1
-#define GMTL_VERSION_PATCH    7
+#define GMTL_VERSION_PATCH    8
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
