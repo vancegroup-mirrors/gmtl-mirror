aefd1313e88aef89a7bd44596dec470372319388
nonchocoboy <nonchocoboy@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2002-09-20 22:08:47 +0000

    Added allenb's Quat fix to the ChangeLog.
    Bumped version to 0.1.9
    
    
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@650 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- gmtl/Version.h
+++ gmtl/Version.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Version.h,v $
- * Date modified: $Date: 2002-08-06 21:12:16 $
- * Version:       $Revision: 1.9 $
+ * Date modified: $Date: 2002-09-20 22:08:47 $
+ * Version:       $Revision: 1.10 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -51,7 +51,7 @@
 // The major/minor/patch version (up to 3 digits each).
 #define GMTL_VERSION_MAJOR    0
 #define GMTL_VERSION_MINOR    1
-#define GMTL_VERSION_PATCH    8
+#define GMTL_VERSION_PATCH    9
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
