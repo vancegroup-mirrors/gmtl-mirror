3b94f3dc905f41f80b8bf3c91e684c6ebdcbf568
nonchocoboy <nonchocoboy@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2003-02-05 22:50:40 +0000

    Still converting over to the new CppUnit.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@699 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- Test/TestSuite/TestCases/EulerAngleClassTest.h
+++ Test/TestSuite/TestCases/EulerAngleClassTest.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: EulerAngleClassTest.h,v $
- * Date modified: $Date: 2002-07-02 02:07:01 $
- * Version:       $Revision: 1.3 $
+ * Date modified: $Date: 2003-02-05 22:50:38 $
+ * Version:       $Revision: 1.4 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -32,33 +32,46 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *
  ************************************************************ ggt-cpr end */
-#include <iostream>
-#include <cppunit/TestCase.h>
-#include <cppunit/TestSuite.h>
-#include <cppunit/TestCaller.h>
+#ifndef _GMTL_EULER_ANGLE_CLASS_TEST_H_
+#define _GMTL_EULER_ANGLE_CLASS_TEST_H_
+
+#include <cppunit/extensions/HelperMacros.h>
 
 namespace gmtlTest
 {
-   class EulerAngleClassTest : public CppUnit::TestCase
+   /**
+    * Functionality tests
+    */
+   class EulerAngleClassTest : public CppUnit::TestFixture
    {
+      CPPUNIT_TEST_SUITE(EulerAngleClassTest);
+
+      CPPUNIT_TEST(testEulerAngleClassTestCreation);
+
+      CPPUNIT_TEST_SUITE_END();
+
    public:
-      EulerAngleClassTest( std::string name = "EulerAngleClassTest" )
-         : CppUnit::TestCase( name )
-      {
-      }
-
-      virtual ~EulerAngleClassTest()
-      {}
-
-      virtual void setUp()
-      {
-      }
-
-      virtual void tearDown()
-      {
-      }
-
       void testEulerAngleClassTestCreation();
+   };
+
+   /**
+    * Metric tests.
+    */
+   class EulerAngleClassMetricTest : public CppUnit::TestFixture
+   {
+      CPPUNIT_TEST_SUITE(EulerAngleClassMetricTest);
+
+      CPPUNIT_TEST(testEulerAngleTimingDefaultConstructor);
+      CPPUNIT_TEST(testEulerAngleTimingElementConstructor);
+      CPPUNIT_TEST(testEulerAngleTimingCopyConstructor);
+      CPPUNIT_TEST(testEulerAngleTimingSet);
+      CPPUNIT_TEST(testEulerAngleTimingOpBracket);
+      CPPUNIT_TEST(testEulerAngleTimingGetData);
+      CPPUNIT_TEST(testEulerAngleTimingOpEqual);
+
+      CPPUNIT_TEST_SUITE_END();
+
+   public:
       void testEulerAngleTimingDefaultConstructor();
       void testEulerAngleTimingElementConstructor();
       void testEulerAngleTimingCopyConstructor();
@@ -66,37 +79,7 @@ namespace gmtlTest
       void testEulerAngleTimingOpBracket();
       void testEulerAngleTimingGetData();
       void testEulerAngleTimingOpEqual();
-
-      static CppUnit::Test* suite()
-      {
-         CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("EulerAngleClassTest");
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleClassTestCreation", &EulerAngleClassTest::testEulerAngleClassTestCreation));
-         return test_suite;
-      }
-
-      static CppUnit::Test* perfSuite()
-      {
-         CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("EulerTiming");
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingDefaultConstructor", &EulerAngleClassTest::testEulerAngleTimingDefaultConstructor));
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingElementConstructor", &EulerAngleClassTest::testEulerAngleTimingElementConstructor));
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingCopyConstructor", &EulerAngleClassTest::testEulerAngleTimingCopyConstructor));
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingSet", &EulerAngleClassTest::testEulerAngleTimingSet));
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingOpBracket", &EulerAngleClassTest::testEulerAngleTimingOpBracket));
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingGetData", &EulerAngleClassTest::testEulerAngleTimingGetData));
-         test_suite->addTest( new CppUnit::TestCaller<EulerAngleClassTest>("testEulerAngleTimingOpEqual", &EulerAngleClassTest::testEulerAngleTimingOpEqual));
-         
-         return test_suite;
-      }
-
-      static CppUnit::Test* interactiveSuite()
-      {
-         CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("InteractiveEulerAngleClassTest");
-         //test_suite->addTest( new CppUnit::TestCaller<ThreadTest>("interactiveCPUGrind", &ThreadTest::interactiveTestCPUGrind));
-         return test_suite;
-      }
-
-   protected:
-
    };
+}
 
-} // end namespace
+#endif
--- Test/TestSuite/TestCases/LineSegTest.cpp
+++ Test/TestSuite/TestCases/LineSegTest.cpp
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: LineSegTest.cpp,v $
- * Date modified: $Date: 2002-12-02 00:08:11 $
- * Version:       $Revision: 1.5 $
+ * Date modified: $Date: 2003-02-05 22:50:40 $
+ * Version:       $Revision: 1.6 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -33,10 +33,32 @@
 *
  ************************************************************ ggt-cpr end */
 #include "LineSegTest.h"
+#include "../Suites.h"
+#include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/extensions/MetricRegistry.h>
+
+#include <gmtl/LineSegOps.h>
+#include <gmtl/Intersection.h>
 
 namespace gmtlTest
 {
+   CPPUNIT_TEST_SUITE_REGISTRATION(LineSegTest);
+   CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(LineSegMetricTest, Suites::metric());
+
+   void LineSegTest::setUp()
+   {
+      origin.set( 0,0,0 );
+      x1_v.set( 1,0,0 );
+      y1_v.set( 0,1,0 );
+      z1_v.set( 0,0,1 );
+      x1_pt.set( 1,0,0 );
+      y1_pt.set( 0,1,0 );
+      z1_pt.set( 0,0,1 );
+
+      x1_lineseg = gmtl::LineSeg<float>(origin, x1_v);
+      y1_lineseg = gmtl::LineSeg<float>(origin, y1_v);
+      z1_lineseg = gmtl::LineSeg<float>(origin, z1_v);
+   }
    // ------------------------
    // CREATION TESTS
    // ------------------------
@@ -261,7 +283,7 @@ namespace gmtlTest
       CPPUNIT_ASSERT(test_lineseg1.getLength() == gmtl::length(x1_v));
    }
 
-   void LineSegTest::testTimingGetLength()
+   void LineSegMetricTest::testTimingGetLength()
    {
       gmtl::LineSeg<float> lineseg;
       const long iters(400000);
@@ -391,4 +413,279 @@ namespace gmtlTest
       CPPUNIT_METRIC_STOP_TIMING();
       CPPUNIT_ASSERT_METRIC_TIMING_LE("LineSegTest/isEqualOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
    }
+
+
+   void LineSegTest::intersectLineSegPlane()
+   {
+      float t;
+      bool result;
+      gmtl::LineSeg<float> lineseg( gmtl::Point<float, 3>( 0,1,0 ), gmtl::Point<float, 3>( 0,-1,0 ) );
+      gmtl::Plane<float> plane( gmtl::Vec<float, 3>( 0,1,0 ), 0 );
+      result = gmtl::intersect( plane, lineseg, t );
+      CPPUNIT_ASSERT( t == 0.5 && result == true );
+   }
+
+   void LineSegTest::intersectLineSegTri()
+   {
+      // test t...
+      {
+         bool result;
+         gmtl::LineSeg<float> l( gmtl::Point3f( 0,1,0 ), gmtl::Point3f( 0,-1,0 ) );
+         gmtl::Tri<float> tri( gmtl::Point3f( -1,0,1 ), gmtl::Point3f( 1,0,1 ), gmtl::Point3f( 0,0,-1 ) );
+         float u, v, t;
+         result = gmtl::intersect( tri, l, u, v, t );
+         CPPUNIT_ASSERT( t == 0.5 && result == true );
+      }
+      // test u and v...
+      {
+         bool result;
+         gmtl::LineSeg<float> l( gmtl::Point3f( 0.25,1,-0.25 ), gmtl::Point3f( 0.25,-1,-0.25 ) );
+         gmtl::Tri<float> tri( gmtl::Point3f( 0,0,0 ), gmtl::Point3f( 1,0,0 ), gmtl::Point3f( 0,0,-1 ) );
+         float u, v, t;
+         result = gmtl::intersect( tri, l, u, v, t );
+         CPPUNIT_ASSERT( t == 0.5 && result == true );
+         CPPUNIT_ASSERT( u == 0.25 && v == 0.25 );
+      }
+
+      // test outside of tri
+      {
+         bool result;
+         gmtl::Tri<float> tri( gmtl::Point3f( 0,0,0 ), gmtl::Point3f( 1,0,0 ), gmtl::Point3f( 0,0,-1 ) );
+         float u, v, t;
+         result = gmtl::intersect( tri, gmtl::LineSeg<float>( gmtl::Point3f( -0.25,1,0 ), gmtl::Point3f( -0.25,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( result == false );
+         result = gmtl::intersect( tri, gmtl::LineSeg<float>( gmtl::Point3f( 0,1,0.25 ), gmtl::Point3f( 0,-1,0.25 ) ), u, v, t );
+         CPPUNIT_ASSERT( result == false );
+
+         // right on edge of tri should give intersection
+         result = gmtl::intersect( tri, gmtl::LineSeg<float>( gmtl::Point3f( 0,1,0 ), gmtl::Point3f( 0,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( t == 0.5 && result == true );
+         // right off edge of tri should not
+         result = gmtl::intersect( tri, gmtl::LineSeg<float>( gmtl::Point3f( -0.000001,1,0 ), gmtl::Point3f( -0.000001,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( result == false );
+      }
+      
+
+      // test other planes...
+      {
+         // x/y plane
+         bool result;
+         float u, v, t;
+         result = gmtl::intersect( gmtl::Tri<float>( gmtl::Point3f( -1,0,0 ), 
+                                                      gmtl::Point3f( 0,0,0 ), 
+                                                      gmtl::Point3f( 0,1,0 ) ), 
+                                   gmtl::LineSeg<float>( gmtl::Point3f( 0,0,1 ), gmtl::Point3f( 0,0,0 ) ), 
+                                   u, v, t );
+         CPPUNIT_ASSERT( t == 1.0 && result == true );
+
+         // try flipped about the x/y plane
+         result = gmtl::intersect( gmtl::Tri<float>( gmtl::Point3f( -1,0,0 ), 
+                                                      gmtl::Point3f( -2,0,0 ), 
+                                                      gmtl::Point3f( -1,1,0 ) ), 
+                                   gmtl::LineSeg<float>( gmtl::Point3f( -1.5,0,-1 ), gmtl::Point3f( -1.5,0,1 ) ), 
+                                   u, v, t );
+         CPPUNIT_ASSERT( t == 0.5 && result == true );
+
+         // make sure backfacing tri's don't intersect...
+         result = gmtl::intersect( gmtl::Tri<float>( gmtl::Point3f( -1,0,0 ), 
+                                                      gmtl::Point3f( -2,0,0 ), 
+                                                      gmtl::Point3f( -1,1,0 ) ), 
+                                   gmtl::LineSeg<float>( gmtl::Point3f( -1.5,0,1 ), gmtl::Point3f( -1.5,0,-1 ) ), 
+                                   u, v, t );
+         CPPUNIT_ASSERT( result == false );
+      }
+   }
+
+   void LineSegTest::intersectRayPlane()
+   {
+      bool result;
+      gmtl::Ray<float> r( gmtl::Point3f( 0,1,0 ), gmtl::Vec3f( 0,-1,0 ) );
+      gmtl::Planef plane( gmtl::Vec3f( 0,1,0 ), 0 );
+      float t;
+      result = gmtl::intersect( plane, r, t );
+      CPPUNIT_ASSERT( t == 1.0 && result == true );
+   }
+
+   void LineSegTest::intersectRayTri()
+   {
+      // test t
+      {
+         bool result;
+         gmtl::Ray<float> r( gmtl::Point3f( 0,1,0 ), gmtl::Vec3f( 0,-1,0 ) );
+         gmtl::Tri<float> tri( gmtl::Point3f( -1,0,1 ), gmtl::Point3f( 1,0,1 ), gmtl::Point3f( 0,0,-1 ) );
+         float u, v, t;
+         result = gmtl::intersect( tri, r, u, v, t );
+         CPPUNIT_ASSERT( t == 1.0 && result == true );
+      }
+
+      // test u and v...
+      {
+         bool result;
+         gmtl::Ray<float> l( gmtl::Point3f( 0.25,1,-0.25 ), gmtl::Vec3f( 0,-1,0 ) );
+         gmtl::Tri<float> tri( gmtl::Point3f( 0,0,0 ), gmtl::Point3f( 1,0,0 ), gmtl::Point3f( 0,0,-1 ) );
+         float u, v, t;
+         result = gmtl::intersect( tri, l, u, v, t );
+         CPPUNIT_ASSERT( t == 1.0 && result == true );
+         CPPUNIT_ASSERT( u == 0.25 && v == 0.25 );
+      }
+
+      // test outside of tri
+      {
+         bool result;
+         gmtl::Tri<float> tri( gmtl::Point3f( 0,0,0 ), gmtl::Point3f( 1,0,0 ), gmtl::Point3f( 0,0,-1 ) );
+         float u, v, t;
+         result = gmtl::intersect( tri, gmtl::Ray<float>( gmtl::Point3f( -0.25,1,0 ), gmtl::Vec3f( 0,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( result == false );
+         result = gmtl::intersect( tri, gmtl::Ray<float>( gmtl::Point3f( 0,1,0.25 ), gmtl::Vec3f( 0,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( result == false );
+
+         // right on edge of tri should give intersection
+         result = gmtl::intersect( tri, gmtl::Ray<float>( gmtl::Point3f( 0,1,0 ), gmtl::Vec3f( 0,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( t == 1.0 && result == true );
+         // right off edge of tri should not
+         result = gmtl::intersect( tri, gmtl::Ray<float>( gmtl::Point3f( -0.000001,1,0 ), gmtl::Vec3f( 0,-1,0 ) ), u, v, t );
+         CPPUNIT_ASSERT( result == false );
+      }
+      
+
+      // test other planes...
+      {
+         // x/y plane
+         bool result;
+         float u, v, t;
+         result = gmtl::intersect( gmtl::Tri<float>( gmtl::Point3f( -1,0,0 ), 
+                                                      gmtl::Point3f( 0,0,0 ), 
+                                                      gmtl::Point3f( 0,1,0 ) ), 
+                                   gmtl::Ray<float>( gmtl::Point3f( 0,0,1 ), gmtl::Vec3f( 0,0,-1 ) ), 
+                                   u, v, t );
+         CPPUNIT_ASSERT( t == 1.0 && result == true );
+
+         // try flipped about the x/y plane
+         result = gmtl::intersect( gmtl::Tri<float>( gmtl::Point3f( -1,0,0 ), 
+                                                      gmtl::Point3f( -2,0,0 ), 
+                                                      gmtl::Point3f( -1,1,0 ) ), 
+                                   gmtl::Ray<float>( gmtl::Point3f( -1.5,0,-1 ), gmtl::Vec3f( 0,0,1 ) ), 
+                                   u, v, t );
+         CPPUNIT_ASSERT( t == 1.0 && result == true );
+
+         // make sure backfacing tri's don't intersect...
+         result = gmtl::intersect( gmtl::Tri<float>( gmtl::Point3f( -1,0,0 ), 
+                                                      gmtl::Point3f( -2,0,0 ), 
+                                                      gmtl::Point3f( -1,1,0 ) ), 
+                                   gmtl::Ray<float>( gmtl::Point3f( -1.5,0,1 ), gmtl::Vec3f( 0,0,-1 ) ), 
+                                   u, v, t );
+         CPPUNIT_ASSERT( result == false );
+      }
+   }
+/*
+   void LineSegTest::testDistance()
+   {
+      gmtl::Plane<float> test_plane( x1_v, 25.0f );
+      CPPUNIT_ASSERT( gmtl::distance(test_plane, origin) == -25.0f );
+      CPPUNIT_ASSERT( gmtl::distance(test_plane, x1_pt) == -24.0f );
+
+      CPPUNIT_ASSERT( gmtl::distance(xy_plane, z1_pt) == 1.0f );
+
+      gmtl::Point<float, 3> pt(-12.0f, 5.0f, -17.0f);
+      CPPUNIT_ASSERT( gmtl::distance(xy_plane, pt) == -17.0f );
+
+      gmtl::Plane<float> slanted_plane( gmtl::Vec<float, 3>(1.0f,1.0f,1.0f), origin );
+      CPPUNIT_ASSERT( gmtl::distance(slanted_plane, origin) == 0.0f );
+
+      pt.set(1.0f, 1.0f, 1.0f );
+      CPPUNIT_ASSERT( gmtl::distance(slanted_plane, pt) > 0.0f );
+
+      // Test distance performance
+      const long iters(400000);
+      float use_value(0.0f);
+
+      // -- Equality
+      CPPUNIT_METRIC_START_TIMING();
+
+      for( long iter=0;iter<iters; ++iter)
+      {
+         test_plane.mOffset += 1.0f;
+         use_value = use_value + gmtl::distance(test_plane, y1_pt);
+      }
+
+      CPPUNIT_METRIC_STOP_TIMING();
+      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/DistanceOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
+   }
+
+   void LineSegTest::testWhichSide()
+   {
+      gmtl::Plane<float>::Side answer;
+
+      answer = gmtl::whichSide(xy_plane, gmtl::Point<float, 3>(0.0,0.0,1.0));
+      CPPUNIT_ASSERT( answer == gmtl::Plane<float>::POS_SIDE );
+
+      answer = gmtl::whichSide(xy_plane, gmtl::Point<float, 3>(0.0,0.0,-12.0f));
+      CPPUNIT_ASSERT( answer == gmtl::Plane<float>::NEG_SIDE );
+
+      answer = gmtl::whichSide(zx_plane, gmtl::Point<float, 3>(0.0,1e-10f,0.0f));
+      CPPUNIT_ASSERT( answer == gmtl::Plane<float>::POS_SIDE );
+
+      answer = gmtl::whichSide(zx_plane, gmtl::Point<float, 3>(0.0,-1e-10f,0.0f));
+      CPPUNIT_ASSERT( answer == gmtl::Plane<float>::NEG_SIDE );
+
+      answer = gmtl::whichSide(xy_plane, gmtl::Point<float, 3>(0.0,0.0,0.0));
+      CPPUNIT_ASSERT( answer == gmtl::Plane<float>::ON_PLANE );
+
+      // Test whichSide performance
+      const long iters(400000);
+      bool true_count(0);
+
+      // -- Equality
+      CPPUNIT_METRIC_START_TIMING();
+
+      for( long iter=0; iter<iters; ++iter)
+      {
+         if ( gmtl::distance(xy_plane, y1_pt) == gmtl::Plane<float>::POS_SIDE )
+            ++true_count;
+      }
+
+      CPPUNIT_METRIC_STOP_TIMING();
+      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/WhichSideOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
+   }
+*/
+   /*
+   void LineSegTest::testFindNearestPt()
+   {
+      gmtl::Point<float, 3> answer, test_point, correct_result;
+      gmtl::Plane<float> xy_plane( gmtl::Vec<float, 3>( 0,1,0 ), 0 );
+      float dist;
+
+      // XY dist to point off origin
+      test_point = gmtl::Point<float, 3>(0.0,0.0,1.0);
+      CPPUNIT_ASSERT(gmtl::distance(xy_plane, test_point) == gmtl::findNearestPt(xy_plane, test_point, answer));
+      CPPUNIT_ASSERT(answer == origin);
+
+      // XY dist to point at 12,21
+      test_point = gmtl::Point<float, 3>(12.0,-21.0,-13.0);
+      correct_result = gmtl::Point<float, 3>(12.0,-21.0,0.0);
+      CPPUNIT_ASSERT( gmtl::distance(xy_plane, test_point) == gmtl::findNearestPt(xy_plane, test_point, answer));
+      CPPUNIT_ASSERT( answer == correct_result);
+
+      // XY dist to point on plane at -17.05, 0.334
+      test_point = gmtl::Point<float, 3>(-17.05,0.334,0.0);
+      CPPUNIT_ASSERT( gmtl::distance(xy_plane, test_point) == gmtl::findNearestPt(xy_plane, test_point, answer));
+      CPPUNIT_ASSERT( answer == test_point);
+
+      // Test findNearestPt performance
+      const long iters(400000);
+      float use_value(0.0f);
+
+      // -- Equality
+      CPPUNIT_METRIC_START_TIMING();
+
+      for( long iter=0; iter<iters; ++iter)
+      {
+         dist = gmtl::findNearestPt(xy_plane, test_point, answer);
+         use_value = use_value + dist + answer[0];
+      }
+
+      CPPUNIT_METRIC_STOP_TIMING();
+      CPPUNIT_ASSERT_METRIC_TIMING_LE("PlaneTest/FindNearestPtOverhead", iters, 0.075f, 0.1f);  // warn at 7.5%, error at 10%
+   }
+   */
 }
