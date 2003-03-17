29eb1f6cbf7c837196e00f3673a9a8e8b33529de
subatomic <subatomic@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2003-03-17 02:13:36 +0000

    fixed compile errir in isEqual( AABox, AABox, eps )
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@777 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/AABoxOps.h
+++ GGT/modules/GMTL/gmtl/AABoxOps.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: AABoxOps.h,v $
- * Date modified: $Date: 2003-03-03 00:54:04 $
- * Version:       $Revision: 1.3 $
+ * Date modified: $Date: 2003-03-17 02:13:36 $
+ * Version:       $Revision: 1.4 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -93,9 +93,9 @@ template< class DATA_TYPE >
 inline bool isEqual( const AABox<DATA_TYPE>& b1, const AABox<DATA_TYPE>& b2, const DATA_TYPE& eps )
 {
    gmtlASSERT( eps >= 0 );
-   return ( (b1.isEmpty() == b2.isEmpty())
-            (isEqual(b1.getMin(), b2.getMin(), eps)) &&
-            (isEqual(b1.getMax(), b2.getMax(), eps)) );
+   return b1.isEmpty() == b2.isEmpty() &&
+          isEqual( b1.getMin(), b2.getMin(), eps ) &&
+          isEqual( b1.getMax(), b2.getMax(), eps );
 }
 /** @} */
 
