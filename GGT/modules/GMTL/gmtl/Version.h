65a202c024eb639273f1a8d6b306bd541e20d9da
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2003-08-15 22:05:51 +0000

    Updated version to 0.3.2 since gmtl::Tri<T> had its interface extended at some point.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@850 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/Version.h
+++ GGT/modules/GMTL/gmtl/Version.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Version.h,v $
- * Date modified: $Date: 2003-04-25 12:50:55 $
- * Version:       $Revision: 1.20 $
+ * Date modified: $Date: 2003-08-15 22:05:50 $
+ * Version:       $Revision: 1.21 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -51,7 +51,7 @@
 // The major/minor/patch version (up to 3 digits each).
 #define GMTL_VERSION_MAJOR    0
 #define GMTL_VERSION_MINOR    3
-#define GMTL_VERSION_PATCH    1
+#define GMTL_VERSION_PATCH    2
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
