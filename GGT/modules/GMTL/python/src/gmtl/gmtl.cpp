bf4c94beb3005285733ac945a6d35b08adeafefa
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2003-08-18 22:51:35 +0000

    Oops, the call to _gmtl_intersect_wrappers_h() was accidentally removed in the last revision.  This prevented gmtl.intersect() from being available.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@866 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/python/src/gmtl/gmtl.cpp
+++ GGT/modules/GMTL/python/src/gmtl/gmtl.cpp
@@ -19,8 +19,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: gmtl.cpp,v $
- * Date modified: $Date: 2003-08-17 06:32:59 $
- * Version:       $Revision: 1.2 $
+ * Date modified: $Date: 2003-08-18 22:51:35 $
+ * Version:       $Revision: 1.3 $
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
@@ -97,6 +97,7 @@ void _Export_gmtl_TriOps_h();
 void _Export_gmtl_QuatOps_h();
 void _Export_gmtl_VecOps_h();
 void _Export_gmtl_functions_h();
+void _Export_gmtl_intersect_wrappers_h();
 void _Export_gmtl_LineSegOps_h();
 void _Export_gmtl_Containment_h();
 void _Export_gmtl_Generate_h();
@@ -175,6 +176,7 @@ BOOST_PYTHON_MODULE(gmtl)
     _Export_gmtl_QuatOps_h();
     _Export_gmtl_VecOps_h();
     _Export_gmtl_functions_h();
+    _Export_gmtl_intersect_wrappers_h();
     _Export_gmtl_LineSegOps_h();
     _Export_gmtl_Containment_h();
     _Export_gmtl_Generate_h();
