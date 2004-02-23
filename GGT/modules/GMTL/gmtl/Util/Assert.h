bf8d6e310b4d62b9aab4dfe4d66df27bc34729a0
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2004-02-23 17:11:06 +0000

    Added missing copyright.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@909 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/Util/Assert.h
+++ GGT/modules/GMTL/gmtl/Util/Assert.h
@@ -1,3 +1,37 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: Assert.h,v $
+ * Date modified: $Date: 2004-02-23 17:11:06 $
+ * Version:       $Revision: 1.2 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
 #ifndef GMTL_ASSERT_H
 #define GMTL_ASSERT_H
 
