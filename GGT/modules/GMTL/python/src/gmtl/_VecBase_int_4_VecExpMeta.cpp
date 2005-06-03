87c45ea875b3e640c4e53e8866135bae38d1ea71
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2005-06-03 15:13:04 +0000

    Use the correct copyright header.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@1048 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/python/src/gmtl/_VecBase_int_4_VecExpMeta.cpp
+++ GGT/modules/GMTL/python/src/gmtl/_VecBase_int_4_VecExpMeta.cpp
@@ -1,29 +1,37 @@
-/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+/************************************************************** ggt-head beg
  *
- * PyJuggler is (C) Copyright 2002, 2003 by Patrick Hartling
+ * GGT: Generic Graphics Toolkit
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Original Authors:
+ *   Allen Bierbaum
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: _VecBase_int_4_VecExpMeta.cpp,v $
- * Date modified: $Date: 2005-06-02 21:22:35 $
- * Version:       $Revision: 1.1 $
+ * Date modified: $Date: 2005-06-03 15:13:04 $
+ * Version:       $Revision: 1.2 $
  * -----------------------------------------------------------------
  *
- *************** <auto-copyright.pl END do not edit this line> ***************/
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
 
 // Includes ====================================================================
 #include <boost/python.hpp>
