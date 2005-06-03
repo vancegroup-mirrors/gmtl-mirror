25f3b36b3eec6bed92e3d54ed08907f1845610fb
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2005-06-03 15:07:11 +0000

    Expose gmtl::Vec2d, gmtl::Vec2f, and gmtl::Vec2i to Python.  I am not sure that these are 100% useful, but what do I know?
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@1047 6382ae23-ed94-47d5-a97d-470f0bc4535f

---
+++ python/src/gmtl/_Vec_int_2.cpp
@@ -0,0 +1,66 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: _Vec_int_2.cpp,v $
+ * Date modified: $Date: 2005-06-03 15:07:11 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
+
+// Includes ====================================================================
+#include <boost/python.hpp>
+#include <gmtl/Vec.h>
+#include <gmtl-pickle.h>
+
+// Using =======================================================================
+using namespace boost::python;
+
+// Module ======================================================================
+void _Export_Vec_int_2()
+{
+    scope* gmtl_Vec_int_2_scope = new scope(
+    class_< gmtl::Vec<int,2>, bases< gmtl::VecBase<int,2> >  >("Vec2i", init<  >())
+        .def(init< const gmtl::Vec<int,2> & >())
+        .def(init< const gmtl::VecBase<int,2> & >())
+        .def(init< const gmtl::VecBase<int,2,gmtl::meta::VecBinaryExpr<gmtl::VecBase<int,2>, gmtl::VecBase<int,2>, gmtl::meta::VecPlusBinary> > & >())
+        .def(init< const gmtl::VecBase<int,2,gmtl::meta::VecBinaryExpr<gmtl::VecBase<int,2>, gmtl::VecBase<int,2>, gmtl::meta::VecMinusBinary> > & >())
+        .def(init< const gmtl::VecBase<int,2,gmtl::meta::VecBinaryExpr<gmtl::VecBase<int,2>, gmtl::VecBase<int,2>, gmtl::meta::VecMultBinary> > & >())
+        .def(init< const gmtl::VecBase<int,2,gmtl::meta::VecBinaryExpr<gmtl::VecBase<int,2>, gmtl::VecBase<int,2>, gmtl::meta::VecDivBinary> > & >())
+        .def(init< const gmtl::VecBase<int,2,gmtl::meta::VecUnaryExpr<gmtl::VecBase<int,2>, gmtl::meta::VecNegUnary> > & >())
+        .def(init< const int &, const int & >())
+        .def_pickle(gmtlPickle::Vec2_pickle<int>())
+    );
+
+    enum_< gmtl::Vec<int,2>::Params >("Params")
+        .value("Size", gmtl::Vec<int,2>::Size)
+    ;
+
+    delete gmtl_Vec_int_2_scope;
+
+}
