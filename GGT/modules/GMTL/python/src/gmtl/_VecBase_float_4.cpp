e4ac9d8451a3197c5bff54fa45abe7f77c114720
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2005-06-04 17:49:09 +0000

    Removed duplicate .def() calls for the operator+=(gmtl::Vec<>, gmtl::Vec<>) overload.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@1061 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/python/src/gmtl/_VecBase_float_4.cpp
+++ GGT/modules/GMTL/python/src/gmtl/_VecBase_float_4.cpp
@@ -19,8 +19,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: _VecBase_float_4.cpp,v $
- * Date modified: $Date: 2004-10-27 19:01:33 $
- * Version:       $Revision: 1.4 $
+ * Date modified: $Date: 2005-06-04 17:49:09 $
+ * Version:       $Revision: 1.5 $
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
@@ -50,7 +50,6 @@ void _Export_VecBase_float_4()
         .def("__setitem__", (void (*)(gmtl::VecBase<float,4>*, const unsigned, float)) &gmtlWrappers::setArrayElement)
         .def_pickle(gmtlPickle::VecBase4_pickle<float>())
         .def(-self)
-        .def(self += self)
         .def(self += self)
         .def(self + self)
         .def(self -= self)
--- GGT/modules/GMTL/python/src/gmtl/_VecBase_int_3.cpp
+++ GGT/modules/GMTL/python/src/gmtl/_VecBase_int_3.cpp
@@ -19,8 +19,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: _VecBase_int_3.cpp,v $
- * Date modified: $Date: 2004-10-27 19:01:33 $
- * Version:       $Revision: 1.4 $
+ * Date modified: $Date: 2005-06-04 17:49:09 $
+ * Version:       $Revision: 1.5 $
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
@@ -50,7 +50,6 @@ void _Export_VecBase_int_3()
         .def("__setitem__", (void (*)(gmtl::VecBase<int,3>*, const unsigned, int)) &gmtlWrappers::setArrayElement)
         .def_pickle(gmtlPickle::VecBase3_pickle<int>())
         .def(-self)
-        .def(self += self)
         .def(self += self)
         .def(self + self)
         .def(self -= self)
