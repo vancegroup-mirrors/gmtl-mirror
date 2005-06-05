21c0c04117c058538128eebff91925abab4ea388
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2005-06-05 22:20:17 +0000

    Expose gmtl::normal() to Python.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/GGT/modules/GMTL@1084 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- python/src/gmtl/_gmtl_TriOps_h.cpp
+++ python/src/gmtl/_gmtl_TriOps_h.cpp
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: _gmtl_TriOps_h.cpp,v $
- * Date modified: $Date: 2003-08-30 17:22:10 $
- * Version:       $Revision: 1.2 $
+ * Date modified: $Date: 2005-06-05 22:20:17 $
+ * Version:       $Revision: 1.3 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -46,6 +46,9 @@ void _Export_gmtl_TriOps_h()
     def("center", (gmtl::Point<double,3> (*)(const gmtl::Tri<double> &))&gmtl::center);
     def("center", (gmtl::Point<int,3> (*)(const gmtl::Tri<int> &))&gmtl::center);
     def("center", (gmtl::Point<float,3> (*)(const gmtl::Tri<float> &))&gmtl::center);
+    def("normal", (gmtl::Vec<double,3> (*)(const gmtl::Tri<double> &))&gmtl::normal);
+    def("normal", (gmtl::Vec<int,3> (*)(const gmtl::Tri<int> &))&gmtl::normal);
+    def("normal", (gmtl::Vec<float,3> (*)(const gmtl::Tri<float> &))&gmtl::normal);
     def("isEqual", (bool (*)(const gmtl::Tri<float> &, const gmtl::Tri<float> &, const float &))&gmtl::isEqual);
     def("isEqual", (bool (*)(const gmtl::Tri<double> &, const gmtl::Tri<double> &, const double &))&gmtl::isEqual);
 }
