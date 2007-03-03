2cdc3498ca0ff25465db5579f96b7d26e82f7837
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2007-03-03 16:06:53 +0000

    From the submitter: "Added documentation made code simpler."
    Submitted by:	Benjamin Schulz <beschulz at googlemail dot com>
    
    
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@1179 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/Frustum.h
+++ GGT/modules/GMTL/gmtl/Frustum.h
@@ -3,12 +3,12 @@
  * GGT: Generic Graphics Toolkit
  *
  * Original Authors:
- *   Benjamin Schulz
+ * Benjamin Schulz
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Frustum.h,v $
- * Date modified: $Date: 2007-03-02 02:19:35 $
- * Version:       $Revision: 1.1 $
+ * Date modified: $Date: 2007-03-03 16:06:53 $
+ * Version:       $Revision: 1.2 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -31,7 +31,7 @@
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *
- ************************************************************ ggt-cpr end */ 
+ ************************************************************ ggt-cpr end */
 
 #ifndef _GMTL_FRUSTUM_H_
 #define _GMTL_FRUSTUM_H_
@@ -41,6 +41,11 @@
 
 namespace gmtl
 {
+	/**
+	* This class defines a View Frustum Volume as a set of 6 planes
+	*
+	* @ingroup Types
+	*/
 	template <class DATA_TYPE>
 	class Frustum
 	{
@@ -48,84 +53,99 @@ namespace gmtl
 			typedef DATA_TYPE DataType;
 			typedef Frustum<DATA_TYPE> FrustumType;
 
-		public:
+			/** An enum to name the plane indicies. 
+			*  To have you not must remember those numbers
+			*/
+			enum PlaneNames
+			{
+				PLANE_LEFT = 0,		/**< left	clipping plane equals 0 */
+				PLANE_RIGHT = 1,	/**< right	clipping plane equals 1 */
+				PLANE_BOTTOM = 2,	/**< bottom	clipping plane equals 2 */
+				PLANE_TOP = 3,		/**< top	clipping plane equals 3 */
+				PLANE_NEAR = 4,		/**< near	clipping plane equals 4 */
+				PLANE_FAR = 5		/**< far	clipping plane equals 5 */
+			};
+
+			/**
+			* Constructs a new frustum with all planes in default state
+			*/
 			Frustum()
 			{
 			
 			}
 
+			/**
+			* Constructs a new frustum with the given projection matrix.
+			*
+			* @param projMatrix the projection matrix of your camera or light etc. to construct the planes from
+			*/
 			Frustum(const gmtl::Matrix44f & projMatrix)
 			{
 				this->extractPlanes(projMatrix);
 			}
 
-			Frustum(const gmtl::Matrix44f & modelviewMatrix, const gmtl::Matrix44f & projMatrix)
+			/**
+			* Constructs a new frustum with given projection and modelview matricies.
+			* the matricies are multiplied in this order : M = projMatrix * modelviewMatrix.
+			* the planes are than extracted from M.
+			*
+			* @param modelviewMatrix the modelview matrix of you camera or light etc. to construct the planes from
+			* @param projMatrix the projection matrix of your camera or light or what ever
+			*/
+			Frustum(const gmtl::Matrix44f & modelviewMatrix, const
+			gmtl::Matrix44f & projMatrix)
 			{
 				this->extractPlanes(modelviewMatrix, projMatrix);
 			}
 
-			void extractPlanes(const gmtl::Matrix44f & modelviewMatrix, const gmtl::Matrix44f & projMatrix)
+			/**
+			* extracts the planes from the given projection matrix.
+			*
+			* @param projMatrix the projection matrix of you camera or light or what ever
+			*/
+			void extractPlanes(const gmtl::Matrix44f & modelviewMatrix,
+			const gmtl::Matrix44f & projMatrix)
 			{
-				//this->extractPlanes(M);
-				gmtl::Matrix44f M;
-
-				float clip[16];
-				const float * modl = modelviewMatrix.getData();
-				const float * proj = projMatrix.getData();
-
-				clip[ 0] = modl[ 0] * proj[ 0] + modl[ 1] * proj[ 4] + modl[ 2] * proj[ 8] + modl[ 3] * proj[12];
-				clip[ 1] = modl[ 0] * proj[ 1] + modl[ 1] * proj[ 5] + modl[ 2] * proj[ 9] + modl[ 3] * proj[13];
-				clip[ 2] = modl[ 0] * proj[ 2] + modl[ 1] * proj[ 6] + modl[ 2] * proj[10] + modl[ 3] * proj[14];
-				clip[ 3] = modl[ 0] * proj[ 3] + modl[ 1] * proj[ 7] + modl[ 2] * proj[11] + modl[ 3] * proj[15];
-
-				clip[ 4] = modl[ 4] * proj[ 0] + modl[ 5] * proj[ 4] + modl[ 6] * proj[ 8] + modl[ 7] * proj[12];
-				clip[ 5] = modl[ 4] * proj[ 1] + modl[ 5] * proj[ 5] + modl[ 6] * proj[ 9] + modl[ 7] * proj[13];
-				clip[ 6] = modl[ 4] * proj[ 2] + modl[ 5] * proj[ 6] + modl[ 6] * proj[10] + modl[ 7] * proj[14];
-				clip[ 7] = modl[ 4] * proj[ 3] + modl[ 5] * proj[ 7] + modl[ 6] * proj[11] + modl[ 7] * proj[15];
-
-				clip[ 8] = modl[ 8] * proj[ 0] + modl[ 9] * proj[ 4] + modl[10] * proj[ 8] + modl[11] * proj[12];
-				clip[ 9] = modl[ 8] * proj[ 1] + modl[ 9] * proj[ 5] + modl[10] * proj[ 9] + modl[11] * proj[13];
-				clip[10] = modl[ 8] * proj[ 2] + modl[ 9] * proj[ 6] + modl[10] * proj[10] + modl[11] * proj[14];
-				clip[11] = modl[ 8] * proj[ 3] + modl[ 9] * proj[ 7] + modl[10] * proj[11] + modl[11] * proj[15];
-
-				clip[12] = modl[12] * proj[ 0] + modl[13] * proj[ 4] + modl[14] * proj[ 8] + modl[15] * proj[12];
-				clip[13] = modl[12] * proj[ 1] + modl[13] * proj[ 5] + modl[14] * proj[ 9] + modl[15] * proj[13];
-				clip[14] = modl[12] * proj[ 2] + modl[13] * proj[ 6] + modl[14] * proj[10] + modl[15] * proj[14];
-				clip[15] = modl[12] * proj[ 3] + modl[13] * proj[ 7] + modl[14] * proj[11] + modl[15] * proj[15];
-
-				M.set(clip);
-				this->extractPlanes(M);
+				this->extractPlanes(projMatrix * modelviewMatrix);
 			}
 
+			/**
+			* extracts the planes from the given projection and modelview matricies.
+			* the matricies are multiplied in this order : M = projMatrix * modelviewMatrix.
+			* the planes are than extracted from M.
+			*
+			* @param modelviewMatrix the modelview matrix of you camera or light etc. to construct the planes from
+			* @param projMatrix the projection matrix of you camera or light etc. to construct the planes from
+			*/
 			void extractPlanes(const gmtl::Matrix44f & projMatrix)
 			{
 				const gmtl::Matrix44f & m = projMatrix;
-				
+
 				//left
-				this->m_planes[0].setNormal( gmtl::Vec3f( m[3][0] + m[0][0], m[3][1] + m[0][1], m[3][2] + m[0][2] ) );
-				this->m_planes[0].setOffset(m[3][3] + m[0][3]);
+				this->m_planes[PLANE_LEFT].setNormal( gmtl::Vec3f( m[3][0] + m[0][0], m[3][1] + m[0][1], m[3][2] + m[0][2] ) );
+				this->m_planes[PLANE_LEFT].setOffset(m[3][3] + m[0][3]);
 				//right
-				this->m_planes[1].setNormal( gmtl::Vec3f( m[3][0] - m[0][0], m[3][1] - m[0][1], m[3][2] - m[0][2] ) );
-				this->m_planes[1].setOffset(m[3][3] - m[0][3]);
+				this->m_planes[PLANE_RIGHT].setNormal( gmtl::Vec3f( m[3][0] - m[0][0], m[3][1] - m[0][1], m[3][2] - m[0][2] ) );
+				this->m_planes[PLANE_RIGHT].setOffset(m[3][3] - m[0][3]);
 				//bottom
-				this->m_planes[2].setNormal( gmtl::Vec3f( m[3][0] + m[1][0], m[3][1] + m[1][1], m[3][2] + m[1][2] ) );
-				this->m_planes[2].setOffset(m[3][3] + m[1][3]);
+				this->m_planes[PLANE_BOTTOM].setNormal( gmtl::Vec3f( m[3][0] + m[1][0], m[3][1] + m[1][1], m[3][2] + m[1][2] ) );
+				this->m_planes[PLANE_BOTTOM].setOffset(m[3][3] + m[1][3]);
 				//top
-				this->m_planes[3].setNormal( gmtl::Vec3f( m[3][0] - m[1][0], m[3][1] - m[1][1], m[3][2] - m[1][2] ) );
-				this->m_planes[3].setOffset(m[3][3] - m[1][3]);
+				this->m_planes[PLANE_TOP].setNormal( gmtl::Vec3f( m[3][0] - m[1][0], m[3][1] - m[1][1], m[3][2] - m[1][2] ) );
+				this->m_planes[PLANE_TOP].setOffset(m[3][3] - m[1][3]);
 				//near
-				this->m_planes[4].setNormal( gmtl::Vec3f( m[3][0] + m[2][0], m[3][1] + m[2][1], m[3][2] + m[2][2] ) );
-				this->m_planes[4].setOffset(m[2][3] + m[3][3]);
+				this->m_planes[PLANE_NEAR].setNormal( gmtl::Vec3f( m[3][0] + m[2][0], m[3][1] + m[2][1], m[3][2] + m[2][2] ) );
+				this->m_planes[PLANE_NEAR].setOffset(m[2][3] + m[3][3]);
 				//far
-				this->m_planes[5].setNormal( gmtl::Vec3f( m[3][0] - m[2][0], m[3][1] - m[2][1], m[3][2] - m[2][2] ) );
-				this->m_planes[5].setOffset(m[3][3] - m[2][3]);
+				this->m_planes[PLANE_FAR].setNormal( gmtl::Vec3f( m[3][0] - m[2][0], m[3][1] - m[2][1], m[3][2] - m[2][2] ) );
+				this->m_planes[PLANE_FAR].setOffset(m[3][3] - m[2][3]);
 			}
 
 			gmtl::Plane<DATA_TYPE> m_planes[6];
 	};
 
-    typedef Frustum<float> Frustumf;
-    typedef Frustum<double> Frustumd;
+	typedef Frustum<float> Frustumf;
+	typedef Frustum<double> Frustumd;
 }
 
 
