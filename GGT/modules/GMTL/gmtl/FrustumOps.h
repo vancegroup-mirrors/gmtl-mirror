ebb70135a4b98a72881076fa595230e3d57d746e
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2007-06-13 16:38:15 +0000

    Moved the gmtl::isInVolume() overloads for gmtl::Frustum<T> to Containment.h.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@1192 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/FrustumOps.h
+++ GGT/modules/GMTL/gmtl/FrustumOps.h
@@ -7,8 +7,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: FrustumOps.h,v $
- * Date modified: $Date: 2007-03-02 02:19:35 $
- * Version:       $Revision: 1.1 $
+ * Date modified: $Date: 2007-06-13 16:38:15 $
+ * Version:       $Revision: 1.2 $
  * -----------------------------------------------------------------
  *
  *********************************************************** ggt-head end */
@@ -61,88 +61,6 @@ namespace gmtl
 			f.m_planes[i].setOffset(o);
 		}
 	}
-
-	template<typename T>
-	inline bool isInVolume(const Frustum<T> & f, const Point<T,3> & p, unsigned int & idx /*out*/)
-	{
-		unsigned int i = 0;
-		for (; i < 6; i++)
-		{
-			T dist = dot(f.m_planes[i].mNorm, static_cast< Vec<T, 3> >(p)) + f.m_planes[i].mOffset;
-			if (dist < T(0.0) )
-			{
-				idx = i;
-				return false;
-			}
-		}
-        
-		idx = IN_FRONT_OFF_ALL_PLANES;
-		return true;
-	}
-
-	template<typename T>
-	inline bool isInVolume(const Frustum<T> & f, const Sphere<T> & s)
-	{
-		unsigned int i = 0;
-		for (; i < 6; i++)
-		{
-			T dist = dot(f.m_planes[i].mNorm, static_cast< Vec<T, 3> >(s.getCenter())) + f.m_planes[i].mOffset;
-			if (dist <= -T(s.getRadius()) )
-				return false;
-		}
-
-		return true;
-	}
-
-	template<typename T>
-	inline bool isInVolume(const Frustum<T> & f, const AABox<T> & box)
-	{
-		const Point3f & min = box.getMin();
-		const Point3f & max = box.getMax();
-		Point3f p[8];
-		p[0] = min;
-		p[1] = max;
-		p[2] = Point3f(max[0], min[1], min[2]);
-		p[3] = Point3f(min[0], max[1], min[2]);
-		p[4] = Point3f(min[0], min[1], max[2]);
-		p[5] = Point3f(max[0], max[1], min[2]);
-		p[6] = Point3f(min[0], max[1], max[2]);
-		p[7] = Point3f(max[0], min[1], max[2]);
-
-		unsigned int idx = 6;
-
-		if (isInVolume(f, p[0], idx))
-			return true;
-
-		//now we have the index of the seperating plane int idx, so check if all other points
-		//lie on the backside of this plane too
-
-		unsigned int i = 0;
-		for (i = 1; i < 8; i++)
-		{
-			T dist = dot(f.m_planes[idx].mNorm, static_cast< Vec<T, 3> >(p[i])) + f.m_planes[idx].mOffset;		
-			if (dist > T(0.0))
-				return true;
-		}
-		
-		return false;
-	}
-
-	template<typename T>
-	inline bool isInVolume(const Frustum<T> & f, const Tri<T> & tri)
-	{
-		if (isInVolume(f, tri[0]) )
-			return true;
-
-		if (isInVolume(f, tri[1]) )
-			return true;
-
-		if (isInVolume(f, tri[2]) )
-			return true;
-
-		return false;
-	}
-
 
 }
 
