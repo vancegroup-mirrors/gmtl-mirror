09e1f63d85f6685bb1c5eb7e69133372c8e97bbd
patrickh <patrickh@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2007-06-25 22:47:55 +0000

    Added copyright header.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/trunk@1201 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/modules/GMTL/gmtl/External/OpenSGConvert.h
+++ GGT/modules/GMTL/gmtl/External/OpenSGConvert.h
@@ -1,3 +1,37 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: OpenSGConvert.h,v $
+ * Date modified: $Date: 2007-06-25 22:47:55 $
+ * Version:       $Revision: 1.10 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
 #ifndef GMTL_OPENSG_CONVERT_H_
 #define GMTL_OPENSG_CONVERT_H_
 
