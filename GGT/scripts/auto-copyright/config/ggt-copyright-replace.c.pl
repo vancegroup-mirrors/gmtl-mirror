55647e98e20ef631ff7e64ce77b10ee047dd7c2f
(no author) <(no author)@6382ae23-ed94-47d5-a97d-470f0bc4535f>
2007-06-26 21:03:30 +0000

    This commit was manufactured by cvs2svn to create tag 'gmtl-release-0-5-3'.
    git-svn-id: https://ggt.svn.sourceforge.net/svnroot/ggt/tags/gmtl-release-0-5-3@1212 6382ae23-ed94-47d5-a97d-470f0bc4535f

--- GGT/scripts/auto-copyright/config/ggt-copyright-replace.c.pl
+++
@@ -1,7 +0,0 @@
-# This is what GGT source code uses for the enclosed copyright block.
-
-$begintag = '/*************************************************************** ggt-cpr beg';
-$endtag =   ' ************************************************************ ggt-cpr end */';
-
-$newbegintag = "$begintag";
-$newendtag = "$endtag";
